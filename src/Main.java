083774dbf375ecc865d6d97f45aa7f2a8181b40b
KimDaJung <ekwjd324@gmail.com>
2021-07-25 00:32:44 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(""));
+        System.out.println(solution.solution(45));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,26 @@
-public class Solution {
-    public String solution(String hand) {
-        String answer = "";
-        return answer;
+class Solution {
+    public int solution(int n) {
+        String reverse3Scale = change3Scale(n).reverse().toString();
+        return (int) change10Scale(String.valueOf(Long.parseLong(reverse3Scale)));
+    }
+
+    //    10진법 -> 3진법으로 바꾸기
+    private StringBuilder change3Scale(long number) {
+        StringBuilder number3Scale = new StringBuilder();
+        while (number > 0) {
+            number3Scale.insert(0, (number % 3));
+            number /= 3;
+        }
+        return number3Scale;
+    }
+
+    //    3진법 -> 10진법으로 바꾸기
+    private long change10Scale(String number) {
+        long number10Scale = 0;
+        while (number != null && number.length() > 0) {
+            number10Scale += (number.charAt(0) - '0') * Math.pow(3, number.length() - 1);
+            number = number.substring(1);
+        }
+        return number10Scale;
     }
 }
