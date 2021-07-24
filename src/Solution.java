9f549219cf7fc811616a3bfc4a1380bb05dedbc5
KimDaJung <ekwjd324@gmail.com>
2021-07-25 00:04:47 +0900

    :tada: solve problem

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,27 @@
-public class Solution {
-    public String solution(String hand) {
-        String answer = "";
-        return answer;
+class Solution {
+    public int solution(int left, int right) {
+        int sum = 0;
+
+        for (int i = left; i <= right; i++) {
+            int divisorCount = findDivisor(i);
+            if (divisorCount % 2 == 0) {
+                sum += i;
+                continue;
+            }
+            sum -= i;
+        }
+
+        return sum;
+    }
+
+    //    약수 찾아서 개수 보여주기
+    private int findDivisor(int inputNumber) {
+        int divisorCount = 0;
+        for (int i = 1; i <= inputNumber; i++) {
+            if (inputNumber % i == 0) {
+                divisorCount++;
+            }
+        }
+        return divisorCount;
     }
 }
