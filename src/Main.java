67fdc4720fb64f4fbf8966ffa2d8174e0c6852b1
KimDaJung <ekwjd324@gmail.com>
2021-07-25 02:00:39 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(""));
+        System.out.println(Arrays.toString(solution.solution(new int[]{44, 1, 0, 0, 31, 25}, new int[]{31, 10, 45, 1, 6, 19})));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,24 @@
-public class Solution {
-    public String solution(String hand) {
-        String answer = "";
-        return answer;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int correctCount = 0;
+        List numbers = Arrays.stream(lottos).boxed().collect(Collectors.toList());
+        for (int num : win_nums) {
+            if (numbers.contains(num)) {
+                correctCount++;
+            }
+        }
+        return new int[]{getLottoRanking(correctCount+Collections.frequency(numbers, 0)), getLottoRanking(correctCount)};
+    }
+
+    private int getLottoRanking(int correctNum) {
+        if (correctNum == 0) {
+            correctNum = 1;
+        }
+        return 7 - correctNum;
     }
 }
