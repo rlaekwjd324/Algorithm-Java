334cbfa6f1db72f93ecb499b6043da4a229c1b01
KimDaJung <ekwjd324@gmail.com>
2021-07-25 01:14:10 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(""));
+        System.out.println(solution.solution(new int[]{3,3,3,2,2,4}));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,11 @@
-public class Solution {
-    public String solution(String hand) {
-        String answer = "";
-        return answer;
+import java.util.HashSet;
+
+class Solution {
+    public int solution(int[] nums) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int num : nums) {
+            hashSet.add(num);
+        }
+        return Math.min(nums.length / 2, hashSet.size());
     }
 }
