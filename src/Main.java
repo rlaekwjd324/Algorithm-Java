a12470ed3df3fa0a3f529585b860ae2d9b10616a
KimDaJung <ekwjd324@gmail.com>
2021-07-26 21:35:11 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String inputWord = bufferedReader.readLine();
+        System.out.print(inputWord.length());
     }
 }
