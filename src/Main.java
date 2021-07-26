7315e3ecd3504654fca5bf63525fd4a6fface294
KimDaJung <ekwjd324@gmail.com>
2021-07-26 16:50:27 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stringBuilder = new StringBuilder();
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int A = Integer.parseInt(stringTokenizer.nextToken());
+        int B = Integer.parseInt(stringTokenizer.nextToken());
+        do {
+            stringBuilder.append(A+B).append("\n");
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            A = Integer.parseInt(stringTokenizer.nextToken());
+            B = Integer.parseInt(stringTokenizer.nextToken());
+        } while (A != 0 || B != 0);
+        System.out.print(stringBuilder);
     }
 }
