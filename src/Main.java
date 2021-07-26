6176f0d49311ffb2b92d495c38a14b212c8924ee
KimDaJung <ekwjd324@gmail.com>
2021-07-26 17:47:41 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int maxNum = Integer.parseInt(bufferedReader.readLine());
+        int maxNumCount = 1;
+        for (int i = 2; i <= 9; i++) {
+            int N = Integer.parseInt(bufferedReader.readLine());
+            if(maxNum<N){
+                maxNum = N;
+                maxNumCount = i;
+            }
+        }
+        System.out.print(maxNum+"\n"+maxNumCount);
     }
 }
