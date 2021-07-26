9f49f6d16c7ef1f1b56555f8001315e9cfd6dda5
KimDaJung <ekwjd324@gmail.com>
2021-07-26 21:52:48 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] word = bufferedReader.readLine().split("-");
+        for (String wordFirstAlphabet : word) {
+            System.out.print(wordFirstAlphabet.charAt(0));
+        }
     }
 }
