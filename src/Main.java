805f9f19db70a513c628737392c1b5a10f282b60
KimDaJung <ekwjd324@gmail.com>
2021-07-26 18:14:37 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int A = Integer.parseInt(stringTokenizer.nextToken());
+        int B = Integer.parseInt(stringTokenizer.nextToken());
+        if (A > B) {
+            System.out.print(">");
+            return;
+        }
+        if (A == B) {
+            System.out.print("==");
+            return;
+        }
+        System.out.print("<");
     }
 }
