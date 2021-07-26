0beb4c6e254470ca7273a536ce883d9a46e16271
KimDaJung <ekwjd324@gmail.com>
2021-07-26 21:45:05 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int count = 0;
+        for (int i = 0; i < 8; i++) {
+            String lineString = bufferedReader.readLine();
+            for (int j = 0; j < 8; j++) {
+                if (lineString.charAt(j) == 'F' && (i + j) % 2 == 0) {
+                    count++;
+                }
+            }
+        }
+        System.out.print(count);
     }
 }
