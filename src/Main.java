9a187607d42a360830e1c9160d8cfb245f9eb865
KimDaJung <ekwjd324@gmail.com>
2021-07-26 18:21:08 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int inputYear = Integer.parseInt(bufferedReader.readLine());
+        if ((inputYear % 4 == 0 && inputYear % 100 != 0) || inputYear % 400 == 0) {
+            System.out.print(1);
+            return;
+        }
+        System.out.print(0);
     }
 }
