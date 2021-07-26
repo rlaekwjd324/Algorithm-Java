b56ef75d16e0670431a336ddaf4e1329b6cd4619
KimDaJung <ekwjd324@gmail.com>
2021-07-26 19:56:05 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            sum += Math.pow(Integer.parseInt(stringTokenizer.nextToken()), 2);
+        }
+        System.out.print(sum % 10);
     }
 }
