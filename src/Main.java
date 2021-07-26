8fa4ef8abc53b5d9ebdf382e33e9662fd23cf96c
KimDaJung <ekwjd324@gmail.com>
2021-07-26 19:14:08 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(bufferedReader.readLine());
+        int sum = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < K; i++) {
+            int number = Integer.parseInt(bufferedReader.readLine());
+            if (number == 0 && !stack.empty()) {
+                stack.pop();
+                continue;
+            }
+            stack.push(number);
+        }
+        for (int number : stack) {
+            sum += number;
+        }
+        System.out.print(sum);
     }
 }
