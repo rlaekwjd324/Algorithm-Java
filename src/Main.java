323ff3bc3010a3206eee3972ddd52021e83a49b9
KimDaJung <ekwjd324@gmail.com>
2021-07-26 20:35:32 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        String A = stringTokenizer.nextToken();
+        String B = stringTokenizer.nextToken();
+        if (A.length() > B.length()) {
+            String temp = A;
+            A = B;
+            B = temp;
+        }
+        int[] sumArray;
+        int upNumber = 0;
+        sumArray = new int[B.length()];
+        for (int i = 1; i <= B.length(); i++) {
+            char bValue = B.charAt(B.length() - i);
+            if (i <= A.length()) {
+                int sum = A.charAt(A.length() - i) - '0' + bValue - '0';
+                sum = sum + upNumber;
+                upNumber = sum / 10;
+                sum = sum % 10;
+                sumArray[B.length() - i] = sum;
+            } else {
+                int sum = upNumber + (bValue - '0');
+                upNumber = sum / 10;
+                sum = sum % 10;
+                sumArray[B.length() - i] = sum;
+            }
+        }
+        if (upNumber > 0) {
+            System.out.print(upNumber);
+        }
+        for (int i = 0; i < sumArray.length; i++) {
+            System.out.print(sumArray[i]);
+        }
     }
 }
