37a61cac5631fc75b2cee6342481e2e21cd2a034
KimDaJung <ekwjd324@gmail.com>
2021-07-27 16:15:03 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int K = Integer.parseInt(stringTokenizer.nextToken());
+        int answer=1;
+        for (int i = 1; i <= K; i++) {
+            answer=(N-K+i)*answer;
+            answer=answer/i;
+        }
+        System.out.print(answer);
     }
 }
