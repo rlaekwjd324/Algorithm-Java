3a5a25828bb6c876bbd0608101cb82cfabbd7820
KimDaJung <ekwjd324@gmail.com>
2021-08-02 01:04:22 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 2 * N - 1; i++) {
+            for (int j = 0; j <= N; j++) {
+                if (j == N || N - j <= Math.abs(i - N + 1)) {
+                    stringBuilder.append("\n");
+                    break;
+                }
+                stringBuilder.append("*");
+            }
+        }
+        System.out.print(stringBuilder);
     }
 }
