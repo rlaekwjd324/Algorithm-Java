93d5a0f1c1ab30c76d1858287624816b18f1b55a
KimDaJung <ekwjd324@gmail.com>
2021-08-02 01:40:19 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        double A = Double.parseDouble(stringTokenizer.nextToken());
+        double B = Double.parseDouble(stringTokenizer.nextToken());
+        System.out.print((int) Math.ceil(A * (B - 0.99)));
     }
 }
