80eee9721f881d6fd0f5cc818234eb5c0279acda
KimDaJung <ekwjd324@gmail.com>
2021-08-02 01:27:31 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int[][] rectangle = new int[4][2];
+        for (int i = 0; i < 3; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            rectangle[i][0] = Integer.parseInt(stringTokenizer.nextToken());
+            rectangle[i][1] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+        for (int i = 0; i < 2; i++) {
+            if (rectangle[0][i] == rectangle[1][i]){
+                rectangle[3][i] = rectangle[2][i];
+            }else if(rectangle[1][i] == rectangle[2][i]){
+                rectangle[3][i] = rectangle[0][i];
+            }else {
+                rectangle[3][i] = rectangle[1][i];
+            }
+        }
+        System.out.print(rectangle[3][0]+" "+rectangle[3][1]);
     }
 }
