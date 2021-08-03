56ab9c3f3e333b433fb21148c8b542f858cc69f6
KimDaJung <ekwjd324@gmail.com>
2021-08-03 18:38:11 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(bufferedReader.readLine());
+        int B = Integer.parseInt(bufferedReader.readLine());
+        int C = Integer.parseInt(bufferedReader.readLine());
+        int D = Integer.parseInt(bufferedReader.readLine());
+        int E = Integer.parseInt(bufferedReader.readLine());
+        if(A<0){
+            System.out.print((-A)*C+D+B*E);
+        }else if(A==0){
+            System.out.print(D+B*E);
+        }else {
+            System.out.print((B-A)*E);
+        }
     }
 }
