fa3ade527c73eccef356f0d1ae7caca4050cdc08
KimDaJung <ekwjd324@gmail.com>
2021-08-03 21:36:54 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int M = Integer.parseInt(stringTokenizer.nextToken());
+        int[] baskets = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            baskets[i] = i;
+        }
+        for (int i = 0; i < M; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int A = Integer.parseInt(stringTokenizer.nextToken());
+            int B = Integer.parseInt(stringTokenizer.nextToken());
+            int temp = baskets[A];
+            baskets[A] = baskets[B];
+            baskets[B] = temp;
+        }
+        for (int i = 1; i <= N; i++) {
+            System.out.print(baskets[i] + " ");
+        }
     }
 }
