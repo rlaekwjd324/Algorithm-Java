337bbaad5a3260087177ff3032b761938ddf73b3
KimDaJung <ekwjd324@gmail.com>
2021-08-03 23:31:09 +0900

    :zap: 임시저장

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String departHour = bufferedReader.readLine();
+        int hour = Integer.parseInt(departHour.substring(0, 2));
+        int minute = Integer.parseInt(departHour.substring(3));
+        int time = 120;
+        if ((hour+2 > 7 && hour < 10)) {
+            time-=(hour);
+        } else if (hour+2 > 15 && hour < 17) {
+
+        } else {
+            hour += 2;
+        }
+        String hourString = String.valueOf(hour);
+        String minString = String.valueOf(minute);
+        if (hour / 10 == 0) {
+            hourString = "0" + hourString;
+        }
+        if (minute / 10 == 0) {
+            minString = "0" + minString;
+        }
+        System.out.print(hourString + ":" + minString);
     }
 }
