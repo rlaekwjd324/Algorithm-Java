4e7241fa286f0d0f5aa65229ea569ccf018db870
KimDaJung <ekwjd324@gmail.com>
2021-08-03 22:12:45 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int M = Integer.parseInt(stringTokenizer.nextToken());
+        int L = Integer.parseInt(stringTokenizer.nextToken());
+        int[] array = new int[N+1];
+        int count = 0;
+        array[1]++;
+        int catchingNum = 1;
+        while (true) {
+            if (array[catchingNum] % 2 == 0) {
+                catchingNum = catchingNum - L;
+                if (catchingNum <= 0)
+                    catchingNum += N;
+            } else {
+                catchingNum = catchingNum + L;
+                if (catchingNum > N)
+                    catchingNum -= N;
+            }
+            array[catchingNum]++;
+            count++;
+            if (array[catchingNum] == M) {
+                System.out.print(count);
+                return;
+            }
+        }
     }
 }
