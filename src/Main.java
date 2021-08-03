a2244236b632df58255049b31631540656de482b
KimDaJung <ekwjd324@gmail.com>
2021-08-03 21:04:02 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int X = Integer.parseInt(stringTokenizer.nextToken());
+        int K = Integer.parseInt(stringTokenizer.nextToken());
+        String[] cups = new String[N + 1];
+        cups[X] = "1";
+        for (int i = 0; i < K; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int A = Integer.parseInt(stringTokenizer.nextToken());
+            int B = Integer.parseInt(stringTokenizer.nextToken());
+            if (A == X || B == X) {
+                String temp = cups[A];
+                cups[A] = cups[B];
+                cups[B] = temp;
+                if (A == X) {
+                    X = B;
+                } else {
+                    X = A;
+                }
+            }
+        }
+        System.out.print(Arrays.asList(cups).indexOf("1"));
     }
 }
