a39363dc74a4a8b622e72ebc6ead7ed145ca0f54
KimDaJung <ekwjd324@gmail.com>
2021-08-08 14:36:47 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.print(format.format(date));
     }
 }
