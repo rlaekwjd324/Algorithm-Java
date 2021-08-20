c4d23d21f0d2ac30b7ca3a2a7ff651ad06901331
KimDaJung <ekwjd324@gmail.com>
2021-08-20 18:19:56 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int E = Integer.parseInt(stringTokenizer.nextToken());
+        int S = Integer.parseInt(stringTokenizer.nextToken());
+        int M = Integer.parseInt(stringTokenizer.nextToken());
+        int e = 1, s = 1, m = 1, count = 1;
+        while (true) {
+            if (e == E && s == S && m == M) {
+                System.out.print(count);
+                return;
+            }
+            count++;
+            if (e < 15) {
+                e++;
+            } else {
+                e = 1;
+            }
+            if (s < 28) {
+                s++;
+            } else {
+                s = 1;
+            }
+            if (m < 19) {
+                m++;
+            } else {
+                m = 1;
+            }
+        }
 
     }
 }
