dd73bbdfa3c3dc148daf5f59b4629c936a12f80c
KimDaJung <ekwjd324@gmail.com>
2021-08-23 23:54:59 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    static int N;
+    static boolean[] check;
+    static int[] output;
+    static StringBuilder stringBuilder;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bufferedReader.readLine());
+        stringBuilder = new StringBuilder();
+        output = new int[N];
+        check = new boolean[N];
+        getArray(0);
+        System.out.print(stringBuilder);
+    }
+
+    private static void getArray(int idx) {
+        if (idx == N) {
+            for (int i = 0; i < N; i++) {
+                stringBuilder.append(output[i]).append(" ");
+            }
+            stringBuilder.append("\n");
+        }
+        for (int i = 0; i < N; i++) {
+            if (!check[i]) {
+                check[i] = true;
+                output[idx] = i + 1;
+                getArray(idx + 1);
+                check[i] = false;
+            }
+        }
     }
 }
