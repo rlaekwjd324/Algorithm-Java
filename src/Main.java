fb3bc3adb2af4939b8bb5e3bac93d88b95a81019
KimDaJung <ekwjd324@gmail.com>
2021-08-24 20:14:31 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 1000; i < 10000; i++) {
+            int sum = getDigitSum(10, i);
+            if (sum == getDigitSum(12, i) && sum == getDigitSum(16, i)) {
+                stringBuilder.append(i).append("\n");
+            }
+        }
+        System.out.print(stringBuilder);
+    }
 
+    private static int getDigitSum(int digit, int num) {
+        int n = 0;
+        int n2 = 0;
+        int sum = 0;
+        int min = digit - 1;
+        while (min < num) {
+            n = (num / digit);
+            n2 = (num % digit);
+            num = n;
+            sum += n2;
+        }
+        return sum + n;
     }
 }
