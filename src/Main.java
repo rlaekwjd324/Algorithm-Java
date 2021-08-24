0b26c7c7a5b8f1e6e5967ec190f601ed2aece7c1
KimDaJung <ekwjd324@gmail.com>
2021-08-24 19:31:56 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    static String a, b;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        a = stringTokenizer.nextToken();
+        b = stringTokenizer.nextToken();
+        int min = 1000000000;
+        for (int i = 0; i <= b.length() - a.length(); i++) {
+            int count = 0;
+            for (int j = 0; j < a.length(); j++) {
+                if (a.charAt(j) != b.charAt(i + j)) {
+                    count++;
+                }
+            }
+            min = Math.min(min, count);
+        }
+        System.out.print(min);
     }
 }
