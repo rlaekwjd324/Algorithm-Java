6c8b633d4cf37d8bf3ab150c812dc1014d68c3e6
KimDaJung <ekwjd324@gmail.com>
2021-08-26 00:25:32 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String a = bufferedReader.readLine();
+        if(a.equals("0")){
+            System.out.print("YONSEI");
+        }else{
+            System.out.print("Leading the Way to the Future");
+        }
     }
 }
