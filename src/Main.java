103c9d63aaaef5ce521b4aff74087c683d48812c
KimDaJung <ekwjd324@gmail.com>
2021-09-05 21:19:37 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String string = bufferedReader.readLine();
+        char prev = string.charAt(0);
+        int height = 10;
+        for (int i = 1; i < string.length(); i++) {
+            if (prev == string.charAt(i)) {
+                height += 5;
+            } else {
+                height += 10;
+            }
+            prev = string.charAt(i);
+        }
+        System.out.print(height);
     }
 }
