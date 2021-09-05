50a2d106f3eda786b5895c1a6638a7984508aa55
KimDaJung <ekwjd324@gmail.com>
2021-09-05 20:57:10 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int a = Integer.parseInt(stringTokenizer.nextToken());
+        int b = Integer.parseInt(stringTokenizer.nextToken());
+        int idx = 1;
+        int sum = 0;
+        int value = 0;
+        while (true) {
+            if (idx > b) {
+                System.out.print(sum);
+                return;
+            }
+            value++;
+            for (int i = 0; i < value; i++) {
+                if (idx > b) {
+                    break;
+                }
+                if (idx >= a) {
+                    sum += value;
+                }
+                idx++;
+            }
+        }
     }
 }
