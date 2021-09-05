defde23ef4fb100316811c7a71db3a10197dd382
KimDaJung <ekwjd324@gmail.com>
2021-09-05 21:00:12 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(bufferedReader.readLine() + "??!");
     }
 }
