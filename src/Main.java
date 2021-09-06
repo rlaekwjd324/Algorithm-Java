a97f328e485d19567cbb58d4647e0cb64c952768
KimDaJung <ekwjd324@gmail.com>
2021-09-07 01:17:42 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
+public class Main {
+    static int N, max = 0;
+    static ArrayList<Integer> array;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bufferedReader.readLine());
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        array = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            array.add(Integer.parseInt(stringTokenizer.nextToken()));
+        }
+        back(0);
+        System.out.println(max);
+    }
+
+    private static void back(int sum) {
+        if (array.size() <= 2) {
+            max = Math.max(max, sum);
+            return;
+        }
+        for (int idx = 1; idx < array.size() - 1; idx++) {
+            int a = array.get(idx);
+            array.remove(idx);
+            back(sum + array.get(idx - 1) * array.get(idx));
+            array.add(idx, a);
+        }
     }
 }
