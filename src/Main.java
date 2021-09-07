aaf4d879cbe4705e0f2431ca293d199d04668791
KimDaJung <ekwjd324@gmail.com>
2021-09-07 20:01:56 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
+public class Main {
+    static int N;
+    static int[] array = {1, 5, 10, 50};
+    static ArrayList<Integer> arrayList = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bufferedReader.readLine());
+        back(0, 0, 0);
+        System.out.print(arrayList.size());
+    }
+
+    private static void back(int idx, int count, int sum) {
+        if (count == N) {
+            if (!arrayList.contains(sum)) {
+                arrayList.add(sum);
+            }
+            return;
+        }
+        for (int i = idx; i < array.length; i++) {
+            back(i, count + 1, sum + array[i]);
+        }
     }
 }
