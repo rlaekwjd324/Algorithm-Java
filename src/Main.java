eea4d78bedb6bd97cf4e871868dc6cdd857dbcfc
KimDaJung <ekwjd324@gmail.com>
2021-09-11 15:08:08 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
+        Solution.solution(437674, 3);
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,26 @@
 public class Solution {
-    public String solution(int[] numbers, String hand) {
-        String answer = "";
+    public static int solution(int n, int k) {
+        int answer = 0;
+        String newN = "";
+        while (n > 0) {
+            newN = (n % k) + newN;
+            n = n / k;
+        }
+        String[] numbers = newN.split("0");
+        for (String number : numbers) {
+            if (number.equals("") || number.equals("1")) {
+                continue;
+            }
+            int divNumCount = 0;
+            for (int j = 2; j <= Math.sqrt(Long.parseLong(number)); j++) {
+                if (Long.parseLong(number) % j == 0) {
+                    divNumCount++;
+                }
+            }
+            if (divNumCount == 0) {
+                answer++;
+            }
+        }
         return answer;
     }
 }
