1f718647ce77f35911a8a27877b2f3d1b7093390
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-02-07 21:25:42 +0900

    :tada: solved problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(stk.nextToken());
+		long B = Long.parseLong(stk.nextToken());
+		System.out.println((A+B)*(A-B));
     }
 }
