392e33fb424e8cebeb832dc6c60ab32135f27cad
KimDaJung <ekwjd324@gmail.com>
2022-02-08 15:56:34 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int roundNumber = Integer.parseInt(bufferedReader.readLine());
+        int changYeongSum = 100;
+        int sangDuckSum = 100;
+        for (int i = 0; i < roundNumber; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int firstNumber = Integer.parseInt(stringTokenizer.nextToken());
+            int secondNumber = Integer.parseInt(stringTokenizer.nextToken());
+            if (firstNumber > secondNumber) {
+                sangDuckSum -= firstNumber;
+            } else if (firstNumber < secondNumber) {
+                changYeongSum -= secondNumber;
+            }
+        }
+        System.out.println(changYeongSum);
+        System.out.println(sangDuckSum);
     }
 }
