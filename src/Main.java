49d014a2616fe4a194ab7b2be9e37ac90b172cdb
KimDaJung <ekwjd324@gmail.com>
2022-02-12 23:30:22 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int firstNumber = Integer.parseInt(stringTokenizer.nextToken());
+        int secondNumber = Integer.parseInt(stringTokenizer.nextToken());
+        StringBuilder stringBuilder = new StringBuilder();
+        while (firstNumber != 0 || secondNumber != 0) {
+            if (firstNumber > secondNumber) {
+                stringBuilder.append("Yes");
+            } else {
+                stringBuilder.append("No");
+            }
+            stringBuilder.append("\n");
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            firstNumber = Integer.parseInt(stringTokenizer.nextToken());
+            secondNumber = Integer.parseInt(stringTokenizer.nextToken());
+        }
+        System.out.print(stringBuilder);
     }
 }
