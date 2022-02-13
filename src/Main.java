9b71180d024f4eefd94396e1f913ca90fb766087
KimDaJung <ekwjd324@gmail.com>
2022-02-13 12:52:14 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(bufferedReader.readLine());
+        int b = Integer.parseInt(bufferedReader.readLine());
+        int c = Integer.parseInt(bufferedReader.readLine());
+        if (a == 60 && b == 60 && c == 60) {
+            System.out.println("Equilateral");
+            return;
+        }
+        if (a + b + c == 180) {
+            if (a != b && b != c && c != a) {
+                System.out.println("Scalene");
+                return;
+            }
+            System.out.println("Isosceles");
+            return;
+        }
+        System.out.println("Error");
     }
 }
