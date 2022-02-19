026332505fad4103a0e6020487cd3173c64ca79d
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-02-19 20:00:51 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class baekjoon {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int x = 0, y = 0, max = 0;
