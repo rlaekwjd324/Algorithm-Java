e3d3f84f80ff1e64edc0901bc5cd282b41ca31c2
KimDaJung <ekwjd324@gmail.com>
2022-02-20 21:08:17 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 1; i <= N; i++) {
+            System.out.println("Hello World, Judge " + i + "!");
+        }
     }
 }
