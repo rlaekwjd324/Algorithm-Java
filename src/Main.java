23926125ffd4c5a470095e0ae2e1f7602760da37
rlaekwjd324 <>
2022-02-21 15:47:30 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 0; i < T; i++) {
+            int y = 0, k = 0;
+            for (int j = 0; j < 9; j++) {
+                StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+                y += Integer.parseInt(stringTokenizer.nextToken());
+                k += Integer.parseInt(stringTokenizer.nextToken());
+            }
+            if (y > k) {
+                System.out.println("Yonsei");
+                continue;
+            }
+            if (y < k) {
+                System.out.println("Korea");
+                continue;
+            }
+            System.out.println("Draw");
+        }
     }
 }
