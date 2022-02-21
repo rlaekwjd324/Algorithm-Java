f35f961331597b7aab86ffdd6d3decab92924f26
KimDaJung <ekwjd324@gmail.com>
2022-02-22 08:57:13 +0900

    :zap: 되돌리기

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(bufferedReader.readLine());
-        for (int i = 0; i < T; i++) {
-            int N = Integer.parseInt(bufferedReader.readLine());
-            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int sum = 0;
-            for (int j = 0; j < N; j++) {
-                sum += Integer.parseInt(stringTokenizer.nextToken());
-            }
-            System.out.println(sum);
-        }
+    public static void main(String[] args) {
+
     }
 }
