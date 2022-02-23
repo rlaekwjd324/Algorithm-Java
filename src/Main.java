943869283aa8e8449143aef1f5ce89f29e69b22e
KimDaJung <ekwjd324@gmail.com>
2022-02-23 20:27:43 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(bufferedReader.readLine());
+        int d = Integer.parseInt(bufferedReader.readLine());
+        if (m == 2) {
+            if (d < 18) {
+                System.out.println("Before");
+                return;
+            }
+            if (d == 18) {
+                System.out.println("Special");
+                return;
+            }
+        }
+        if (m < 2) {
+            System.out.println("Before");
+            return;
+        }
+        System.out.println("After");
     }
 }
