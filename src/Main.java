18bb78b20fcc267103ef2e48c6b4515db49a3c36
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-02-25 13:01:01 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,7 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int s = Integer.parseInt(stringTokenizer.nextToken());
-        int k = Integer.parseInt(stringTokenizer.nextToken());
-        int h = Integer.parseInt(stringTokenizer.nextToken());
-        if (s + k + h >= 100) {
-            System.out.print("OK");
-            return;
-        }
-        if (s > k) {
-            if (k < h) {
-                System.out.print("Korea");
-                return;
-            }
-            System.out.print("Hanyang");
-            return;
-        }
-        if (s > h) {
-            System.out.print("Hanyang");
-            return;
-        }
-        System.out.print("Soongsil");
+        
     }
 
 }
