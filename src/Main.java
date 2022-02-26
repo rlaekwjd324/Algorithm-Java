f4525931ea658fd6c46b831a74123e0ea509535e
KimDaJung <ekwjd324@gmail.com>
2022-02-26 09:26:46 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 0; i < t; i++) {
+            double number = Double.parseDouble(bufferedReader.readLine());
+            System.out.println("$" + String.format("%.2f", number * 0.8));
+        }
     }
 }
+
