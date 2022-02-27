ebb93e5fd6b5ef73c5667bfc77a399993286de11
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-02-27 21:11:06 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(bufferedReader.readLine());
-        for (int i = 0; i < t; i++) {
-            double number = Double.parseDouble(bufferedReader.readLine());
-            System.out.println("$" + String.format("%.2f", number * 0.8));
-        }
-    }
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Avengers: Endgame");
+	}
 
 }
-
