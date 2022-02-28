3f46c9967e5beaaa106f19dd821dcd75591b5112
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-02-28 16:10:34 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Avengers: Endgame");
-	}
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int number = Integer.parseInt(bufferedReader.readLine());
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            int a = Integer.parseInt(stringTokenizer.nextToken());
+            if (a == number) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 }
