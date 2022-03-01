ade51a7d3a7eb8f7f5e669c8e2c163c901d125b2
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-01 09:53:31 +0900

    초기화

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,7 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bufferedReader.readLine());
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int number = Integer.parseInt(bufferedReader.readLine());
-        int count = 0;
-        for (int i = 0; i < N; i++) {
-            int a = Integer.parseInt(stringTokenizer.nextToken());
-            if (a == number) {
-                count++;
-            }
-        }
-        System.out.println(count);
+
     }
 }
