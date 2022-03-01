216e937e5989d7ca4857020d13f5642926475b6b
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-01 09:52:04 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-
+        SimpleDateFormat date = new SimpleDateFormat("yyyy.MM.dd");
+        String timeStamp = date.format(new Date());
+        System.out.println(timeStamp.split("\\.")[0]);
+        System.out.println(timeStamp.split("\\.")[1]);
+        System.out.println(timeStamp.split("\\.")[2]);
     }
 }
