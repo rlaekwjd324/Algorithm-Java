1914f63060efe1ad0746f23054328acd47611f0a
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-04 22:35:59 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bufferedReader.readLine());
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int correctCount = 0;
-        int score = 0;
-        for (int i = 0; i < N; i++) {
-            int a = Integer.parseInt(stringTokenizer.nextToken());
-            if (a == 1) {
-                correctCount++;
-            } else {
-                correctCount = 0;
-            }
-            score += correctCount;
-        }
-        System.out.println(score);
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("A+", "4.3");
+        hashMap.put("A0", "4.0");
+        hashMap.put("A-", "3.7");
+        hashMap.put("B+", "3.3");
+        hashMap.put("B0", "3.0");
+        hashMap.put("B-", "2.7");
+        hashMap.put("C+", "2.3");
+        hashMap.put("C0", "2.0");
+        hashMap.put("C-", "1.7");
+        hashMap.put("D+", "1.3");
+        hashMap.put("D0", "1.0");
+        hashMap.put("D-", "0.7");
+        hashMap.put("F", "0.0");
+        System.out.println(hashMap.get(bufferedReader.readLine()));
     }
 }
