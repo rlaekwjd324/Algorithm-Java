0ab7a78069175c683a8c6b1cda7c47f973fda21a
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-06 00:14:05 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        HashMap<String, String> hashMap = new HashMap<>();
-        hashMap.put("A+", "4.3");
-        hashMap.put("A0", "4.0");
-        hashMap.put("A-", "3.7");
-        hashMap.put("B+", "3.3");
-        hashMap.put("B0", "3.0");
-        hashMap.put("B-", "2.7");
-        hashMap.put("C+", "2.3");
-        hashMap.put("C0", "2.0");
-        hashMap.put("C-", "1.7");
-        hashMap.put("D+", "1.3");
-        hashMap.put("D0", "1.0");
-        hashMap.put("D-", "0.7");
-        hashMap.put("F", "0.0");
-        System.out.println(hashMap.get(bufferedReader.readLine()));
+        int T = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 0; i < T; i++) {
+            int a = Integer.parseInt(bufferedReader.readLine());
+            int count = 0;
+            boolean array[] = new boolean[a];
+            for (int j = 2; j <= a; j++) {
+                for (int k = 1; k <= a / j; k++) {
+                    array[j * k - 1] = !array[j * k - 1];
+                }
+            }
+            for (int j = 0; j < a; j++) {
+                if (!array[j]) {
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
     }
 }
