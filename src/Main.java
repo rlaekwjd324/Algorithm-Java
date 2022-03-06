997d761f049e1149abfd9b8e38ef3a3a45d4c8ba
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-06 22:07:55 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int x = Integer.parseInt(stringTokenizer.nextToken());
+        int y = Integer.parseInt(stringTokenizer.nextToken());
+        int w = Integer.parseInt(stringTokenizer.nextToken());
+        int h = Integer.parseInt(stringTokenizer.nextToken());
+        int min = Math.min(Math.abs(x - w), Math.abs(y - h));
+        int min2 = Math.min(x, y);
+        System.out.println(Math.min(min, min2));
     }
 }
