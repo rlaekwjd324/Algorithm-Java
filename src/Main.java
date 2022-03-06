f55849443eac1e482ec5ea54397a44db3d18be81
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-06 22:09:03 +0900

    solve baekjoon-1085

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(bufferedReader.readLine());
-        for (int i = 0; i < T; i++) {
-            int a = Integer.parseInt(bufferedReader.readLine());
-            int count = 0;
-            boolean array[] = new boolean[a];
-            for (int j = 2; j <= a; j++) {
-                for (int k = 1; k <= a / j; k++) {
-                    array[j * k - 1] = !array[j * k - 1];
-                }
-            }
-            for (int j = 0; j < a; j++) {
-                if (!array[j]) {
-                    count++;
-                }
-            }
-            System.out.println(count);
-        }
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int x = Integer.parseInt(stringTokenizer.nextToken());
+        int y = Integer.parseInt(stringTokenizer.nextToken());
+        int w = Integer.parseInt(stringTokenizer.nextToken());
+        int h = Integer.parseInt(stringTokenizer.nextToken());
+        int min = Math.min(Math.abs(x - w), Math.abs(y - h));
+        int min2 = Math.min(x, y);
+        System.out.println(Math.min(min, min2));
     }
 }
