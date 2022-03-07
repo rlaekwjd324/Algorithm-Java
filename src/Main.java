8110fcf71575e0f467d5dfc05fc28caadb431db5
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-07 21:12:17 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int x = Integer.parseInt(stringTokenizer.nextToken());
-        int y = Integer.parseInt(stringTokenizer.nextToken());
-        int w = Integer.parseInt(stringTokenizer.nextToken());
-        int h = Integer.parseInt(stringTokenizer.nextToken());
-        int min = Math.min(Math.abs(x - w), Math.abs(y - h));
-        int min2 = Math.min(x, y);
-        System.out.println(Math.min(min, min2));
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+		long N = Long.parseLong(stringTokenizer.nextToken());
+		long M = Long.parseLong(stringTokenizer.nextToken());
+		System.out.println(N==M ? 1 : 0);
+	}
 }
