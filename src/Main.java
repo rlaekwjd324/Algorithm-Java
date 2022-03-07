515e0882664965e2589886a71444adbb4dfe9b42
KimDaJung <ekwjd324@gmail.com>
2022-03-07 21:10:46 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Avengers: Endgame");
-	}
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+        long N = Long.parseLong(stringTokenizer.nextToken());
+        long M = Long.parseLong(stringTokenizer.nextToken());
+        System.out.println(N == M ? 1 : 0);
+    }
 }
