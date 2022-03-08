820fe7ac2469ae8021c961182da3d888b1316015
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-08 10:15:31 +0900

    solve baekjoon-5522

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
-		long N = Long.parseLong(stringTokenizer.nextToken());
-		long M = Long.parseLong(stringTokenizer.nextToken());
-		System.out.println(N==M ? 1 : 0);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            sum += Integer.parseInt(bufferedReader.readLine());
+        }
+        System.out.println(sum);
+    }
 }
