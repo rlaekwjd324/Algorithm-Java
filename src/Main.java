650bbe46767024a8215e4987cc71513c565e4439
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-10 11:55:05 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stringBuilder = new StringBuilder();
+        String inputNumber = bufferedReader.readLine();
+        while (!inputNumber.equals("0")) {
+            int sum = 1;
+            for (int i = 0; i < inputNumber.length(); i++) {
+                char chr = inputNumber.charAt(i);
+                if (chr == '1') {
+                    sum += 2;
+                } else if (chr == '0') {
+                    sum += 4;
+                } else {
+                    sum += 3;
+                }
+                sum++;
+            }
+            stringBuilder.append(sum).append("\n");
+            inputNumber = bufferedReader.readLine();
+        }
+        System.out.print(stringBuilder);
     }
 }
