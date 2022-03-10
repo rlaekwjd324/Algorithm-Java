92580b9788b465aefb024a255b65532c997f25d2
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-10 11:56:51 +0900

    solve baekjoon-1284

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int[] array = new int[3];
-        for (int i = 0; i < 3; i++) {
-            array[i] = Integer.parseInt(stringTokenizer.nextToken());
+        StringBuilder stringBuilder = new StringBuilder();
+        String inputNumber = bufferedReader.readLine();
+        while (!inputNumber.equals("0")) {
+            int sum = 1;
+            for (int i = 0; i < inputNumber.length(); i++) {
+                char chr = inputNumber.charAt(i);
+                if (chr == '1') {
+                    sum += 2;
+                } else if (chr == '0') {
+                    sum += 4;
+                } else {
+                    sum += 3;
+                }
+                sum++;
+            }
+            stringBuilder.append(sum).append("\n");
+            inputNumber = bufferedReader.readLine();
         }
-        Arrays.sort(array);
-        for (int i = 0; i < 3; i++) {
-            System.out.print(array[i] + " ");
-        }
+        System.out.print(stringBuilder);
     }
 }
