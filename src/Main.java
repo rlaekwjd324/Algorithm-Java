10eaaa3ade03b425c15355c96e45a8b73aa76a81
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-12 19:42:13 +0900

    :tada: solve baekjoon-2163

--- src/Main.java
+++ src/Main.java
@@ -7,23 +7,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int a = Integer.parseInt(stringTokenizer.nextToken());
-        int b = Integer.parseInt(stringTokenizer.nextToken());
-        int c = Integer.parseInt(stringTokenizer.nextToken());
-        if (a == b && b == c) {
-            System.out.println(10000 + a * 1000);
-            return;
-        }
-        if (a == b || a == c) {
-            System.out.println(1000 + 100 * a);
-            return;
-        }
-        if (b == c) {
-            System.out.println(1000 + 100 * b);
-            return;
-        }
-        int max = Math.max(a, b);
-        max = Math.max(max, c);
-        System.out.println(max * 100);
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int M = Integer.parseInt(stringTokenizer.nextToken());
+        System.out.println(N * M - 1);
     }
 }
