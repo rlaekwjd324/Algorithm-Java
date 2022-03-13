dbebf7b52e19713cb99681a904b0b560e048aa9d
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-13 19:57:20 +0900

    :tada: solve baekjoon-5026

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int N = Integer.parseInt(stringTokenizer.nextToken());
-        int M = Integer.parseInt(stringTokenizer.nextToken());
-        System.out.println(N * M - 1);
+        int N = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 0; i < N; i++) {
+            String tempString = bufferedReader.readLine();
+            if (tempString.equals("P=NP")) {
+                System.out.println("skipped");
+                continue;
+            }
+            System.out.println(Integer.parseInt(tempString.split("\\+")[0]) + Integer.parseInt(tempString.split("\\+")[1]));
+        }
     }
 }
