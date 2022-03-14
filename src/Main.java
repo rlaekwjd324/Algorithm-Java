3fd9f0ebe2cfeb6cedd35cb0affe0f654dbb9f88
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-14 11:25:58 +0900

    :tada: solve baekjoon-1075

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,15 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bufferedReader.readLine());
-        for (int i = 0; i < N; i++) {
-            String tempString = bufferedReader.readLine();
-            if (tempString.equals("P=NP")) {
-                System.out.println("skipped");
-                continue;
+        String N = bufferedReader.readLine();
+        int F = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 0; i < 100; i++) {
+            String backNum = String.valueOf(i).length() == 1 ? "0" + i : String.valueOf(i);
+            long num = Long.parseLong(N.substring(0, N.length() - 2) + backNum);
+            if (num % F == 0) {
+                System.out.println(backNum);
+                return;
             }
-            System.out.println(Integer.parseInt(tempString.split("\\+")[0]) + Integer.parseInt(tempString.split("\\+")[1]));
         }
     }
 }
