1993472b907c121c3a191695cc7ded1e7ca659ff
KimDaJung <ekwjd324@gmail.com>
2022-03-14 11:25:10 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String N = bufferedReader.readLine();
+        int F = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 0; i < 100; i++) {
+            String backNum = String.valueOf(i).length() == 1 ? "0" + i : String.valueOf(i);
+            long num = Long.parseLong(N.substring(0, N.length() - 2) + backNum);
+            if (num % F == 0) {
+                System.out.println(backNum);
+                return;
+            }
+        }
     }
 }
