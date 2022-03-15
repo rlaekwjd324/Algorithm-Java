bee1c2b75fcad1a1f807cdc79f8bf5e4a76a830b
KimDaJung <ekwjd324@gmail.com>
2022-03-15 18:50:45 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String line = bufferedReader.readLine();
+        StringBuilder stringBuilder = new StringBuilder();
+        char[] array = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        while (!line.equals("#")) {
+            int count = 0;
+            for (int i = 0; i < line.length(); i++) {
+                for (char c : array) {
+                    if (c == line.charAt(i)) {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            stringBuilder.append(count).append("\n");
+            line = bufferedReader.readLine();
+        }
+        System.out.print(stringBuilder);
     }
 }
