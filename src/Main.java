b312a6482e9d9bed99728e3f3378a44b73835d79
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-15 18:51:29 +0900

    :tada: solve baekjoon-1264

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,22 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String N = bufferedReader.readLine();
-        int F = Integer.parseInt(bufferedReader.readLine());
-        for (int i = 0; i < 100; i++) {
-            String backNum = String.valueOf(i).length() == 1 ? "0" + i : String.valueOf(i);
-            long num = Long.parseLong(N.substring(0, N.length() - 2) + backNum);
-            if (num % F == 0) {
-                System.out.println(backNum);
-                return;
+        String line = bufferedReader.readLine();
+        StringBuilder stringBuilder = new StringBuilder();
+        char[] array = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        while (!line.equals("#")) {
+            int count = 0;
+            for (int i = 0; i < line.length(); i++) {
+                for (char c : array) {
+                    if (c == line.charAt(i)) {
+                        count++;
+                        break;
+                    }
+                }
             }
+            stringBuilder.append(count).append("\n");
+            line = bufferedReader.readLine();
         }
+        System.out.print(stringBuilder);
     }
 }
