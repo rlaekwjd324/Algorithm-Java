9455d31d7f6320cac2fd24eda8ce674ecc55436e
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-16 15:00:06 +0900

    :tada: solve baekjoon-1392

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String line = bufferedReader.readLine();
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int Q = Integer.parseInt(stringTokenizer.nextToken());
         StringBuilder stringBuilder = new StringBuilder();
-        char[] array = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
-        while (!line.equals("#")) {
-            int count = 0;
-            for (int i = 0; i < line.length(); i++) {
-                for (char c : array) {
-                    if (c == line.charAt(i)) {
-                        count++;
-                        break;
-                    }
+        int[] nArray = new int[N];
+        for (int i = 0; i < N; i++) {
+            nArray[i] = Integer.parseInt(bufferedReader.readLine());
+        }
+        for (int i = 0; i < Q; i++) {
+            int qNumber = Integer.parseInt(bufferedReader.readLine());
+            int count = -1;
+            for (int j = 0; j < N; j++) {
+                count = count + nArray[j];
+                if (count >= qNumber) {
+                    stringBuilder.append(j + 1).append("\n");
+                    break;
                 }
             }
-            stringBuilder.append(count).append("\n");
-            line = bufferedReader.readLine();
         }
         System.out.print(stringBuilder);
     }
