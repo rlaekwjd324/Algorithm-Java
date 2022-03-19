b645763d93e270cf53515bf91e608cb73c550fb8
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-20 00:54:51 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bufferedReader.readLine());
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        boolean[] position = new boolean[101];
-        int count = 0;
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(stringTokenizer.nextToken());
-            if (!position[num]) {
-                position[num] = true;
-            } else {
-                count++;
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("-", 0);
+        hashMap.put("\\", 1);
+        hashMap.put("(", 2);
+        hashMap.put("@", 3);
+        hashMap.put("?", 4);
+        hashMap.put(">", 5);
+        hashMap.put("&", 6);
+        hashMap.put("%", 7);
+        hashMap.put("/", -1);
+        String lineString = bufferedReader.readLine();
+        StringBuilder answer = new StringBuilder();
+        while (!lineString.equals("#")) {
+            int sum = 0;
+            for (int i = 0; i < lineString.length(); i++) {
+                String str = String.valueOf(lineString.charAt(i));
+                double num = Math.pow(8, lineString.length() - i - 1) * hashMap.get(str);
+                sum += num;
             }
+            answer.append(sum).append("\n");
+            lineString = bufferedReader.readLine();
         }
-        System.out.println(count);
+        System.out.println(answer);
     }
 }
