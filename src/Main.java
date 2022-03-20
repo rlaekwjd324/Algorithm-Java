a0b2bba96c5e10c54ec3203b07b46e1be861d5e2
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-20 20:18:26 +0900

    :tada: solve baekjoon-2145

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,22 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        HashMap<String, Integer> hashMap = new HashMap<>();
-        hashMap.put("-", 0);
-        hashMap.put("\\", 1);
-        hashMap.put("(", 2);
-        hashMap.put("@", 3);
-        hashMap.put("?", 4);
-        hashMap.put(">", 5);
-        hashMap.put("&", 6);
-        hashMap.put("%", 7);
-        hashMap.put("/", -1);
-        String lineString = bufferedReader.readLine();
-        StringBuilder answer = new StringBuilder();
-        while (!lineString.equals("#")) {
-            int sum = 0;
-            for (int i = 0; i < lineString.length(); i++) {
-                String str = String.valueOf(lineString.charAt(i));
-                double num = Math.pow(8, lineString.length() - i - 1) * hashMap.get(str);
-                sum += num;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String num = br.readLine();
+        StringBuilder sb = new StringBuilder();
+        while (!num.equals("0")) {
+            while (num.length() > 1) {
+                int sum = 0;
+                for (int i = 0; i < num.length(); i++) {
+                    sum += (num.charAt(i) - '0');
+                }
+                num = String.valueOf(sum);
             }
-            answer.append(sum).append("\n");
-            lineString = bufferedReader.readLine();
+            sb.append(num).append("\n");
+            num = br.readLine();
         }
-        System.out.println(answer);
+        System.out.print(sb);
     }
 }
+
