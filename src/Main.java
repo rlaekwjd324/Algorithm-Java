a2dc82cb1d265c77a87c1d62039c8ef48a907607
KimDaJung <ekwjd324@gmail.com>
2022-03-21 20:54:46 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        int count = 0;
+        int num = 0;
+        while (N > 0) {
+            num++;
+            if (N < num) {
+                num = 1;
+            }
+            N -= num;
+            count++;
+        }
+        System.out.println(count);
     }
 }
