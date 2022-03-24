fa2bdb20f8994120cbbb0c50f2f613572d2e5019
KimDaJung <ekwjd324@gmail.com>
2022-03-24 10:23:02 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder str = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            str.append(i);
+        }
+        System.out.print(str.indexOf(String.valueOf(N)) + 1);
     }
 }
