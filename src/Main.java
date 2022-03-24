b7568ba90b60fa60d0439a0eb74a01976617fa03
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-24 10:23:42 +0900

    :tada: solve baekjoon-2154

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bufferedReader.readLine());
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            String str = bufferedReader.readLine();
-            int num = 0;
-            for (int j = 0; j < 3; j++) {
-                int frontNum = str.split("-")[0].charAt(j) - 65;
-                num += Math.pow(26, 3 - j - 1) * frontNum;
-            }
-            int s = Integer.parseInt(str.split("-")[1]);
-            if (Math.abs(num - s) <= 100) {
-                stringBuilder.append("nice").append("\n");
-            } else {
-                stringBuilder.append("not nice").append("\n");
-            }
+        StringBuilder str = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            str.append(i);
         }
-        System.out.print(stringBuilder);
+        System.out.print(str.indexOf(String.valueOf(N)) + 1);
     }
 }
