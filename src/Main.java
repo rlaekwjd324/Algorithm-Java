b8ca26a5bfec2efdbcb49e0fc392e64854f8b8e0
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-25 10:00:24 +0900

    :tada: solve baekjoon-1233

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bufferedReader.readLine());
-        StringBuilder str = new StringBuilder();
-        for (int i = 1; i <= N; i++) {
-            str.append(i);
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int S1 = Integer.parseInt(stringTokenizer.nextToken());
+        int S2 = Integer.parseInt(stringTokenizer.nextToken());
+        int S3 = Integer.parseInt(stringTokenizer.nextToken());
+        int[] array = new int[S1 + S2 + S3 + 1];
+        for (int i = 1; i <= S1; i++) {
+            for (int j = 1; j <= S2; j++) {
+                for (int k = 1; k <= S3; k++) {
+                    array[i + j + k]++;
+                }
+            }
         }
-        System.out.print(str.indexOf(String.valueOf(N)) + 1);
+        int answer = 0;
+        int index = 0;
+        for (int i = 1; i <= S1 + S2 + S3; i++) {
+            if (answer < array[i]) {
+                answer = array[i];
+                index = i;
+            }
+        }
+        System.out.println(index);
     }
 }
