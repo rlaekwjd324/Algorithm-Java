d0f238c75d17f1cf6a6cfb6721caaf887aca9184
KimDaJung <ekwjd324@gmail.com>
2022-03-26 13:33:47 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(bufferedReader.readLine());
+            if (Math.pow(num, 2) % (Math.pow(10, String.valueOf(num).length())) == num) {
+                stringBuilder.append("YES").append("\n");
+            } else {
+                stringBuilder.append("NO").append("\n");
+            }
+        }
+        System.out.print(stringBuilder);
     }
 }
