86ff38d075075f4b6cf00903b31fbd3f940362f2
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-26 13:34:15 +0900

    :tada: solve baekjoon-2028

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int S1 = Integer.parseInt(stringTokenizer.nextToken());
-        int S2 = Integer.parseInt(stringTokenizer.nextToken());
-        int S3 = Integer.parseInt(stringTokenizer.nextToken());
-        int[] array = new int[S1 + S2 + S3 + 1];
-        for (int i = 1; i <= S1; i++) {
-            for (int j = 1; j <= S2; j++) {
-                for (int k = 1; k <= S3; k++) {
-                    array[i + j + k]++;
-                }
+        int N = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(bufferedReader.readLine());
+            if (Math.pow(num, 2) % (Math.pow(10, String.valueOf(num).length())) == num) {
+                stringBuilder.append("YES").append("\n");
+            } else {
+                stringBuilder.append("NO").append("\n");
             }
         }
-        int answer = 0;
-        int index = 0;
-        for (int i = 1; i <= S1 + S2 + S3; i++) {
-            if (answer < array[i]) {
-                answer = array[i];
-                index = i;
-            }
-        }
-        System.out.println(index);
+        System.out.print(stringBuilder);
     }
 }
