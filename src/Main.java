83ba6a6ae5284f2c658785245e04cd64904891f1
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-27 17:09:44 +0900

    :tada: solve baekjoon-2161

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bufferedReader.readLine());
-        StringBuilder stringBuilder = new StringBuilder();
+        ArrayList<Integer> arrayList = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(bufferedReader.readLine());
-            if (Math.pow(num, 2) % (Math.pow(10, String.valueOf(num).length())) == num) {
-                stringBuilder.append("YES").append("\n");
-            } else {
-                stringBuilder.append("NO").append("\n");
+            arrayList.add(i);
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        while (arrayList.size() > 0) {
+            stringBuilder.append(arrayList.get(0) + 1).append(" ");
+            arrayList.remove(0);
+            if (arrayList.size() > 0) {
+                int a = arrayList.get(0);
+                arrayList.remove(0);
+                arrayList.add(a);
             }
         }
         System.out.print(stringBuilder);
