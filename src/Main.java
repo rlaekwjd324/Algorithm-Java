5e38904a9747d66980721a678f6efb45012cf7e8
KimDaJung <ekwjd324@gmail.com>
2022-03-28 19:25:54 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        double d = Double.parseDouble(stringTokenizer.nextToken());
+        double h = Double.parseDouble(stringTokenizer.nextToken());
+        double w = Double.parseDouble(stringTokenizer.nextToken());
+        double num = Math.sqrt(d * d / (h * h + w * w));
+        System.out.println((int) (h * num) + " " + (int) (w * num));
     }
 }
