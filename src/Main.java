4bc0f67ff81b95dc7518c02dbcccb6dde80101b2
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-28 19:26:32 +0900

    :tada: solve baekjoon-1297

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bufferedReader.readLine());
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            arrayList.add(i);
-        }
-        StringBuilder stringBuilder = new StringBuilder();
-        while (arrayList.size() > 0) {
-            stringBuilder.append(arrayList.get(0) + 1).append(" ");
-            arrayList.remove(0);
-            if (arrayList.size() > 0) {
-                int a = arrayList.get(0);
-                arrayList.remove(0);
-                arrayList.add(a);
-            }
-        }
-        System.out.print(stringBuilder);
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        double d = Double.parseDouble(stringTokenizer.nextToken());
+        double h = Double.parseDouble(stringTokenizer.nextToken());
+        double w = Double.parseDouble(stringTokenizer.nextToken());
+        double num = Math.sqrt(d * d / (h * h + w * w));
+        System.out.println((int) (h * num) + " " + (int) (w * num));
     }
 }
