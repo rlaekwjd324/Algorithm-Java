538cdd20dad0c289026b66cd200fefb23ac62caa
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-03-29 11:19:54 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int M = Integer.parseInt(stringTokenizer.nextToken());
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int nSum = 0;
+        for (int i = 0; i < N; i++) {
+            nSum += Integer.parseInt(stringTokenizer.nextToken());
+        }
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int mSum = 0;
+        for (int i = 0; i < M; i++) {
+            mSum += Integer.parseInt(stringTokenizer.nextToken());
+        }
+        System.out.println(Math.max(nSum - mSum, 0));
     }
 }
