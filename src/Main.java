03b5445b92a817fa3a304fac2133d0f599b4691b
KimDaJung <ekwjd324@gmail.com>
2022-04-02 14:37:57 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < K; i++) {
+            int h = Integer.parseInt(bufferedReader.readLine());
+            String actions = bufferedReader.readLine();
+            for (int j = 0; j < actions.length(); j++) {
+                if (h == 0) {
+                    break;
+                }
+                if (actions.charAt(j) == 'c') {
+                    h++;
+                } else {
+                    h--;
+                }
+            }
+            stringBuilder.append("Data Set ").append(i + 1).append(":").append("\n").append(h).append("\n\n");
+        }
+        System.out.print(stringBuilder);
     }
 }
