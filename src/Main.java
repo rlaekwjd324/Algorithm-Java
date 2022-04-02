76696ef4186bc6a659bc63496ba062fabf63e846
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-04-02 14:38:36 +0900

    :tada: solve baekjoon-10205

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,23 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String first = bufferedReader.readLine();
-        String second = bufferedReader.readLine();
-        int[] firstArray = new int[26];
-        int[] secondArray = new int[26];
-        for (int i = 0; i < first.length(); i++) {
-            firstArray[first.charAt(i) - 97]++;
+        int K = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < K; i++) {
+            int h = Integer.parseInt(bufferedReader.readLine());
+            String actions = bufferedReader.readLine();
+            for (int j = 0; j < actions.length(); j++) {
+                if (h == 0) {
+                    break;
+                }
+                if (actions.charAt(j) == 'c') {
+                    h++;
+                } else {
+                    h--;
+                }
+            }
+            stringBuilder.append("Data Set ").append(i + 1).append(":").append("\n").append(h).append("\n\n");
         }
-        for (int i = 0; i < second.length(); i++) {
-            secondArray[second.charAt(i) - 97]++;
-        }
-        int count = 0;
-        for (int i = 0; i < 26; i++) {
-            count += Math.abs(firstArray[i] - secondArray[i]);
-        }
-        System.out.println(count);
+        System.out.print(stringBuilder);
     }
 }
