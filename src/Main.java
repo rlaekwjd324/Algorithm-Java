23b2e4cffb4abf532f84c19a915cf4548e80364b
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-04-04 15:44:18 +0900

    :tada: solve baekjoon-9610

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bufferedReader.readLine());
-        if (N % 2 == 0) {
-            System.out.print(((N + 2) / 2) * ((N + 2) / 2));
-            return;
+        int n = Integer.parseInt(bufferedReader.readLine());
+        int[] array = new int[5];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int x = Integer.parseInt(stringTokenizer.nextToken());
+            int y = Integer.parseInt(stringTokenizer.nextToken());
+            if(x == 0 || y == 0){
+                array[0]++;
+                continue;
+            }
+            if(x>0&&y>0){
+                array[1]++;
+                continue;
+            }
+            if(x>0 && y<0){
+                array[4]++;
+                continue;
+            }
+            if(x<0 && y>0){
+                array[2]++;
+                continue;
+            }
+            if(x<0 && y<0){
+                array[3]++;
+            }
         }
-        System.out.print(((N + 2) / 2) * ((N + 2) / 2 + 1));
+        System.out.println("Q1: "+array[1]);
+        System.out.println("Q2: "+array[2]);
+        System.out.println("Q3: "+array[3]);
+        System.out.println("Q4: "+array[4]);
+        System.out.println("AXIS: "+array[0]);
     }
 }
