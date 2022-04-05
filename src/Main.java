c142c3a0782206bce30f48c5291c5f3b8ec6ccfb
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-04-05 19:16:12 +0900

    :tada: solve baekjoon-2592

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bufferedReader.readLine());
-        int[] array = new int[5];
-        for (int i = 0; i < n; i++) {
-            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int x = Integer.parseInt(stringTokenizer.nextToken());
-            int y = Integer.parseInt(stringTokenizer.nextToken());
-            if(x == 0 || y == 0){
-                array[0]++;
-                continue;
-            }
-            if(x>0&&y>0){
-                array[1]++;
-                continue;
-            }
-            if(x>0 && y<0){
-                array[4]++;
-                continue;
-            }
-            if(x<0 && y>0){
-                array[2]++;
-                continue;
-            }
-            if(x<0 && y<0){
-                array[3]++;
+        int sum = 0;
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        int max = 1;
+        for (int i = 0; i < 10; i++) {
+            int num = Integer.parseInt(bufferedReader.readLine());
+            sum += num;
+            if (hashMap.containsKey(num)) {
+                hashMap.replace(num, hashMap.get(num) + 1);
+                max = Math.max(max, hashMap.get(num));
+            } else {
+                hashMap.put(num, 1);
             }
         }
-        System.out.println("Q1: "+array[1]);
-        System.out.println("Q2: "+array[2]);
-        System.out.println("Q3: "+array[3]);
-        System.out.println("Q4: "+array[4]);
-        System.out.println("AXIS: "+array[0]);
+        int answer = 0;
+        for (int key : hashMap.keySet()) {
+            int value = hashMap.get(key);
+            if (value == max) {
+                answer = key;
+            }
+        }
+        System.out.println(sum / 10);
+        System.out.println(answer);
     }
 }
