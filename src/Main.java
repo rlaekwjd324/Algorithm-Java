172d56365ed468013c44ea06e1b3e3fec80d40da
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-04-05 19:15:11 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        HashMap<Integer, Integer> hashMap = new HashMap();
+        int max = 1;
+        for (int i = 0; i < 10; i++) {
+            int num = Integer.parseInt(bufferedReader.readLine());
+            sum += num;
+            if (hashMap.containsKey(num)) {
+                hashMap.replace(num, hashMap.get(num) + 1);
+                max = Math.max(max, hashMap.get(num));
+            } else {
+                hashMap.put(num, 1);
+            }
+        }
+        int answer = 0;
+        for (int key : hashMap.keySet()) {
+            int value = hashMap.get(key);
+            if (value == max) {
+                answer = key;
+            }
+        }
+        System.out.println(sum / 10);
+        System.out.println(answer);
     }
 }
