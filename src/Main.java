8985529ecdc0c0c95d0f5ea3262a5d26d1c1cbb3
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-04-07 12:40:45 +0900

    :tada: solve baekjoon-13752

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(bufferedReader.readLine());
+        int n = Integer.parseInt(bufferedReader.readLine());
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < T; i++) {
-            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            double num = Double.parseDouble(stringTokenizer.nextToken());
-
-            while (stringTokenizer.hasMoreTokens()) {
-                String a = stringTokenizer.nextToken();
-                switch (a) {
-                    case "@":
-                        num = num * 3;
-                        break;
-                    case "%":
-                        num = num + 5;
-                        break;
-                    case "#":
-                        num = num - 7;
-                        break;
-                }
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(bufferedReader.readLine());
+            for (int j = 0; j < num; j++) {
+                stringBuilder.append("=");
             }
-
-            stringBuilder.append(String.format("%.2f", num)).append("\n");
+            stringBuilder.append("\n");
         }
         System.out.print(stringBuilder);
     }
