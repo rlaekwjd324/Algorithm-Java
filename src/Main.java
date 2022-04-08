609a2609fa169a52688c0c4ca80c77ee7c9101d7
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-04-08 17:42:51 +0900

    :tada: solve baekjoon-17256

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bufferedReader.readLine());
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(bufferedReader.readLine());
-            for (int j = 0; j < num; j++) {
-                stringBuilder.append("=");
-            }
-            stringBuilder.append("\n");
-        }
-        System.out.print(stringBuilder);
+        int A[] = new int[3];
+        int C[] = new int[3];
+
+        StringTokenizer stk = new StringTokenizer(bufferedReader.readLine());
+        A[0] = Integer.parseInt(stk.nextToken());
+        A[1] = Integer.parseInt(stk.nextToken());
+        A[2] = Integer.parseInt(stk.nextToken());
+        stk = new StringTokenizer(bufferedReader.readLine());
+        C[0] = Integer.parseInt(stk.nextToken());
+        C[1] = Integer.parseInt(stk.nextToken());
+        C[2] = Integer.parseInt(stk.nextToken());
+        
+        System.out.print((C[0]-A[2])+" "+(C[1]/A[1])+" "+(C[2]-A[0]));
     }
 }
