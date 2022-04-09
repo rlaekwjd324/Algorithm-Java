b77f2f057bb0e39391780172906994ee0948c280
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-04-09 11:35:21 +0900

    :tada: solve baekjoon-19944

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int A[] = new int[3];
-        int C[] = new int[3];
 
-        StringTokenizer stk = new StringTokenizer(bufferedReader.readLine());
-        A[0] = Integer.parseInt(stk.nextToken());
-        A[1] = Integer.parseInt(stk.nextToken());
-        A[2] = Integer.parseInt(stk.nextToken());
-        stk = new StringTokenizer(bufferedReader.readLine());
-        C[0] = Integer.parseInt(stk.nextToken());
-        C[1] = Integer.parseInt(stk.nextToken());
-        C[2] = Integer.parseInt(stk.nextToken());
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(stk.nextToken());
+        int M = Integer.parseInt(stk.nextToken());
         
-        System.out.print((C[0]-A[2])+" "+(C[1]/A[1])+" "+(C[2]-A[0]));
+        if(M == 1 || M == 2) {
+            System.out.print("NEWBIE!");
+            return;
+        }
+        if(N >= M) {
+            System.out.print("OLDBIE!");
+            return;
+        }
+        System.out.print("TLE!");
     }
 }
