9beb9dfa8a4874dcc05e3bb538db1d269b08be6d
KimDaJung <ekwjd324@gmail.com>
2022-04-10 18:51:48 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        int min = 100;
+        for (int i = 0; i < 4; i++) {
+            int score = Integer.parseInt(bufferedReader.readLine());
+            min = Math.min(min, score);
+            sum += score;
+        }
+        sum -= min;
+        int historyScore = Integer.parseInt(bufferedReader.readLine());
+        int geographyScore = Integer.parseInt(bufferedReader.readLine());
+        int max = Math.max(historyScore, geographyScore);
+        sum += max;
+        System.out.print(sum);
     }
 }
