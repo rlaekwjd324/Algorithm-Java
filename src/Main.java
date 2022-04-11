34068971272ff1d8894a8c596ff33b84476f9fc5
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-04-11 13:15:29 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stringBuilder = new StringBuilder();
+        int testcaseCount = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 0; i < testcaseCount; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            char[] firstArray = stringTokenizer.nextToken().toCharArray();
+            char[] secondArray = stringTokenizer.nextToken().toCharArray();
+            stringBuilder.append("Distances:");
+            for (int j = 0; j < firstArray.length; j++) {
+                if (firstArray[j] > secondArray[j]) {
+                    stringBuilder.append(" ").append((int) secondArray[j] + 26 - (int) firstArray[j]);
+                } else {
+                    stringBuilder.append(" ").append((int) secondArray[j] - (int) firstArray[j]);
+                }
+            }
+            stringBuilder.append("\n");
+        }
+        System.out.print(stringBuilder);
     }
 }
