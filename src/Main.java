7261269199ae461e4a30ad69fd7487dd11a6c509
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-04-13 11:56:29 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stringBuilder = new StringBuilder();
+        int N = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 0; i < N; i++) {
+            stringBuilder.append("god");
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            stringTokenizer.nextToken();
+            while (stringTokenizer.hasMoreTokens()) {
+                stringBuilder.append(stringTokenizer.nextToken());
+            }
+            stringBuilder.append("\n");
+        }
+        System.out.print(stringBuilder);
     }
 }
