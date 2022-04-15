81c9eeeb872bdf8920509d98f9979a8b0024284e
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-04-15 16:37:07 +0900

    :tada: solve baekjoon-5523

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String num = bufferedReader.readLine();
-        int leftSum = 0;
-        int rightSum = 0;
-        for (int i = 0; i < num.length() / 2; i++) {
-            leftSum = leftSum + (num.charAt(i) - '0');
-            rightSum = rightSum + (num.charAt(num.length() - i - 1) - '0');
+        int N = Integer.parseInt(bufferedReader.readLine());
+        int aWinCount = 0;
+        int bWinCount = 0;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int a = Integer.parseInt(stringTokenizer.nextToken());
+            int b = Integer.parseInt(stringTokenizer.nextToken());
+            if (a > b) {
+                aWinCount++;
+            } else if (a < b) {
+                bWinCount++;
+            }
         }
-        if (leftSum == rightSum) {
-            System.out.print("LUCKY");
-            return;
-        }
-        System.out.print("READY");
+        System.out.print(aWinCount + " " + bWinCount);
     }
 }
