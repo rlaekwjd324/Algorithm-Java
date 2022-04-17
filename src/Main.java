579f95380c3a8d35faa47b3736b52f70b3e72367
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-04-17 19:18:18 +0900

    :tada: solve baekjoon-5586

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int N = Integer.parseInt(stringTokenizer.nextToken());
-        int M = Integer.parseInt(stringTokenizer.nextToken());
-        int[][] array = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            String str = bufferedReader.readLine();
-            for (int j = 0; j < M; j++) {
-                if (str.charAt(j) == '1') {
-                    array[i][M - j - 1] = 1;
-                }
+        String str = bufferedReader.readLine();
+        int joiCount = 0;
+        int ioiCount = 0;
+        for (int i = 0; i < str.length() - 2; i++) {
+            String word = str.substring(i, i + 3);
+            if (word.equals("JOI")) {
+                joiCount++;
+            }
+            if (word.equals("IOI")) {
+                ioiCount++;
             }
         }
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                stringBuilder.append(array[i][j]);
-            }
-            stringBuilder.append("\n");
-        }
-        System.out.print(stringBuilder);
+        System.out.println(joiCount);
+        System.out.println(ioiCount);
     }
 }
