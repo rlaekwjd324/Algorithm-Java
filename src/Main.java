0f5827e94cf8976a7c2034fd442f7bbfc69997e5
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-04-18 13:15:23 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int[] array = new int[N];
+        for (int i = 0; i < N; i++) {
+            array[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+        Arrays.sort(array);
+        System.out.print(array[N - 1] - array[0]);
     }
 }
