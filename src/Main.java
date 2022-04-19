de8590bab38f93b72eebbc3ea64b7907c209b8f9
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-04-19 10:51:14 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            String str = bufferedReader.readLine();
+            stringBuilder.append(str.charAt(0)).append(str.charAt(str.length() - 1)).append("\n");
+        }
+        System.out.print(stringBuilder);
     }
 }
