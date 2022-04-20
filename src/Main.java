3fd519da96a0373bbb896697595f776dda45056e
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-04-20 17:27:22 +0900

    :tada: solve baekjoon-3058

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(bufferedReader.readLine());
         StringBuilder stringBuilder = new StringBuilder();
+        int T = Integer.parseInt(bufferedReader.readLine());
         for (int i = 0; i < T; i++) {
-            String str = bufferedReader.readLine();
-            stringBuilder.append(str.charAt(0)).append(str.charAt(str.length() - 1)).append("\n");
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int sum = 0;
+            int min = 100;
+            for (int j = 0; j < 7; j++) {
+                int num = Integer.parseInt(stringTokenizer.nextToken());
+                if (num % 2 == 0) {
+                    sum += num;
+                    min = Math.min(min, num);
+                }
+            }
+            stringBuilder.append(sum).append(" ").append(min).append("\n");
         }
         System.out.print(stringBuilder);
     }
