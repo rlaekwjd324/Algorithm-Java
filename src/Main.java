ec744e7a2a4a24de12e31ec9c59de8188f631426
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-04-21 15:21:09 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stringBuilder = new StringBuilder();
+        int T = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(bufferedReader.readLine());
+            int max = 0;
+            String bestUnv = "";
+            for (int j = 0; j < N; j++) {
+                StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+                String university = stringTokenizer.nextToken();
+                int bottle = Integer.parseInt(stringTokenizer.nextToken());
+                if (max < bottle) {
+                    max = bottle;
+                    bestUnv = university;
+                }
+            }
+            stringBuilder.append(bestUnv).append("\n");
+        }
+        System.out.print(stringBuilder);
     }
 }
