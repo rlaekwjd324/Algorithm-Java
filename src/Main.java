ac134d21fa844328d9af843b648bb4dce0332abd
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-04-22 13:52:12 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String str = bufferedReader.readLine();
+        int sum = 0;
+        for (int i = 0; i < str.split(",").length; i++) {
+            sum += Integer.parseInt(str.split(",")[i]);
+        }
+        System.out.print(sum);
     }
 }
