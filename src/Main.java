90db2d69230715000d986fa6c5a606d216dc3131
KimDaJung <ekwjd324@gmail.com>
2022-04-24 15:01:32 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int A = Integer.parseInt(stringTokenizer.nextToken());
+        int B = Integer.parseInt(stringTokenizer.nextToken());
+        int C = Integer.parseInt(stringTokenizer.nextToken());
+        int D = Integer.parseInt(stringTokenizer.nextToken());
+        System.out.print(Math.abs(A + D - B - C));
     }
 }
