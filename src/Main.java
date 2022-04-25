2f723fe360119663332b08e76e85b60fb0cc20a9
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-04-25 09:58:05 +0900

    :tada: solve baekjoon-24883

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int A = Integer.parseInt(stringTokenizer.nextToken());
-        int B = Integer.parseInt(stringTokenizer.nextToken());
-        int C = Integer.parseInt(stringTokenizer.nextToken());
-        int D = Integer.parseInt(stringTokenizer.nextToken());
-        System.out.print(Math.abs(A + D - B - C));
+        String str = bufferedReader.readLine();
+        if (str.equals("N") || str.equals("n")) {
+            System.out.print("Naver D2");
+            return;
+        }
+        System.out.print("Naver Whale");
     }
 }
