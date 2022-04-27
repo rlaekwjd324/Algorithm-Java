b75e1bcd47de44b94a48a6c7753f216308604c95
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-04-27 13:11:28 +0900

    :tada: solve baekjoon-2738

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,29 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int n = Integer.parseInt(stringTokenizer.nextToken());
-        int m = Integer.parseInt(stringTokenizer.nextToken());
-        int k = Integer.parseInt(stringTokenizer.nextToken());
-        System.out.print(n - (Math.abs(m - k)));
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int M = Integer.parseInt(stringTokenizer.nextToken());
+        int[][] aArray = new int[N][M];
+        int[][] bArray = new int[N][M];
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            for (int j = 0; j < M; j++) {
+                aArray[i][j] = Integer.parseInt(stringTokenizer.nextToken());
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            for (int j = 0; j < M; j++) {
+                bArray[i][j] = Integer.parseInt(stringTokenizer.nextToken());
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                stringBuilder.append(aArray[i][j] + bArray[i][j]).append(" ");
+            }
+            stringBuilder.append("\n");
+        }
+        System.out.print(stringBuilder);
     }
 }
