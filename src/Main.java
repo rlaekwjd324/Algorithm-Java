9aaf0de8b7ccd49e6171a1b24850f3d1e8256982
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-04-28 15:33:22 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stringBuilder = new StringBuilder();
+        int T = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 1; i <= T; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            stringBuilder.append("Case ").append(i).append(": ").append(Integer.parseInt(stringTokenizer.nextToken()) + Integer.parseInt(stringTokenizer.nextToken())).append("\n");
+        }
+        System.out.print(stringBuilder);
     }
 }
