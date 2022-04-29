98babd9559a8b630e10cbe5f0313e99a07f7c8e7
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-04-29 12:03:42 +0900

    :tada: solve baekjoon-11098

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,22 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
         StringBuilder stringBuilder = new StringBuilder();
-        int T = Integer.parseInt(bufferedReader.readLine());
-        for (int i = 1; i <= T; i++) {
-            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            stringBuilder.append("Case ").append(i).append(": ").append(Integer.parseInt(stringTokenizer.nextToken()) + Integer.parseInt(stringTokenizer.nextToken())).append("\n");
+        for (int i = 0; i < n; i++) {
+            int p = Integer.parseInt(bufferedReader.readLine());
+            int max = 0;
+            String expensive = "";
+            for (int j = 0; j < p; j++) {
+                StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+                int num = Integer.parseInt(stringTokenizer.nextToken());
+                String player = stringTokenizer.nextToken();
+                if (num > max) {
+                    expensive = player;
+                    max = num;
+                }
+            }
+            stringBuilder.append(expensive).append("\n");
         }
         System.out.print(stringBuilder);
     }
