d033fca0c7f4f52caef959b0c8122a96c86c1068
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-04-29 12:03:04 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int p = Integer.parseInt(bufferedReader.readLine());
+            int max = 0;
+            String expensive = "";
+            for (int j = 0; j < p; j++) {
+                StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+                int num = Integer.parseInt(stringTokenizer.nextToken());
+                String player = stringTokenizer.nextToken();
+                if (num > max) {
+                    expensive = player;
+                    max = num;
+                }
+            }
+            stringBuilder.append(expensive).append("\n");
+        }
+        System.out.print(stringBuilder);
     }
 }
