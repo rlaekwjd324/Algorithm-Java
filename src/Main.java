2cf1b7cbe0b81572f1f529c61c57f1ab2cf6f779
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-04-30 12:15:23 +0900

    :tada: solve baekjoon-2789

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bufferedReader.readLine());
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            int p = Integer.parseInt(bufferedReader.readLine());
-            int max = 0;
-            String expensive = "";
-            for (int j = 0; j < p; j++) {
-                StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-                int num = Integer.parseInt(stringTokenizer.nextToken());
-                String player = stringTokenizer.nextToken();
-                if (num > max) {
-                    expensive = player;
-                    max = num;
-                }
-            }
-            stringBuilder.append(expensive).append("\n");
+        String letter = bufferedReader.readLine();
+        String remove = "CAMBRIDGE";
+        for (int i = 0; i < remove.length(); i++) {
+            letter = letter.replaceAll(String.valueOf(remove.charAt(i)), "");
         }
-        System.out.print(stringBuilder);
+        System.out.println(letter);
     }
 }
