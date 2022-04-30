c03a6ea887e669c44cdeb406f6c2a7ad02b2e5ff
KimDaJung <ekwjd324@gmail.com>
2022-04-30 12:14:47 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String letter = bufferedReader.readLine();
+        String remove = "CAMBRIDGE";
+        for (int i = 0; i < remove.length(); i++) {
+            letter = letter.replaceAll(String.valueOf(remove.charAt(i)), "");
+        }
+        System.out.println(letter);
     }
 }
