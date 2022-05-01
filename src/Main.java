6830daea698a03b5ca1747541aae643f23c4b281
KimDaJung <ekwjd324@gmail.com>
2022-05-01 16:38:51 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String str = bufferedReader.readLine();
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if ("aeiou".contains(String.valueOf(str.charAt(i)))) {
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
