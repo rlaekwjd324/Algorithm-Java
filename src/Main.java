e7e3dc5f9fcae5dea0d0eb41c664708deacb8a4b
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-03 13:57:27 +0900

    :tada: solve baekjoon-4458

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,15 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String str = bufferedReader.readLine();
+        int N = Integer.parseInt(bufferedReader.readLine());
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            if (c <= 67) {
-                c = (char) (c + 26);
+        for (int i = 0; i < N; i++) {
+            String str = bufferedReader.readLine();
+            if (str.charAt(0) < 97) {
+                stringBuilder.append(str).append("\n");
+            } else {
+                stringBuilder.append((char) (str.charAt(0) - 32)).append(str.substring(1)).append("\n");
             }
-            stringBuilder.append((char) (c - 3));
         }
         System.out.print(stringBuilder);
     }
