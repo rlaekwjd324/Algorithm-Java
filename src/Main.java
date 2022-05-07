71bb818b37f5b22629ff8b8c96003b49a9c78120
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-07 21:59:06 +0900

    :tada: solve baekjoon-11023

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,14 @@
 import java.io.*;
 import java.util.*;
 
-public class Main
-{
-    public static void main(String[] args) throws IOException
-    {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bf.readLine());
-        int[] array = new int[n];
-        int[] sortedArray = new int[n];
-        StringTokenizer stk = new StringTokenizer(bf.readLine());
-        for (int i=0;i<n;i++) {
-            array[i] = Integer.parseInt(stk.nextToken());
-            sortedArray[i] = array[i];
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk = new StringTokenizer(br.readLine());
+        int sum = 0;
+        while(stk.hasMoreTokens()) {
+            sum += Integer.parseInt(stk.nextToken());
         }
-        Arrays.sort(sortedArray);
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            if(array[i] != sortedArray[i]) {
-                answer++;
-            }
-        }
-        System.out.print(answer);
-    }
+        System.out.print(sum);
+	}
 }
