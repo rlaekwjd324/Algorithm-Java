249c1ed156468d3a7f7098d1c219c7e4f49f1385
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-08 23:25:51 +0900

    :tada: solve baekjoon-14581

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,7 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer stk = new StringTokenizer(br.readLine());
-        int sum = 0;
-        while(stk.hasMoreTokens()) {
-            sum += Integer.parseInt(stk.nextToken());
-        }
-        System.out.print(sum);
+		String str = br.readLine();
+        System.out.print(":fan::fan::fan:\n:fan::"+str+"::fan:\n:fan::fan::fan:");
 	}
 }
