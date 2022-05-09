c85104c7790ee10404eb1762dc83cfa9dca2f002
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-09 13:48:25 +0900

    :tada: solve baekjoon-2954

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
-        System.out.print(":fan::fan::fan:\n:fan::"+str+"::fan:\n:fan::fan::fan:");
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String str = bufferedReader.readLine();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+                str = str.substring(0, i) + str.substring(i + 2);
+            }
+        }
+        System.out.print(str);
+    }
 }
