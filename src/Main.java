ddf097cde685a8b10dbf2eea55532e4511a92425
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-09 13:43:20 +0900

    :zap:

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String b1 =bufferedReader.readLine();
+        String b2 = bufferedReader.readLine();
+
+        for (int i = 0; i < b2.length(); i++) {
+            if(b2.charAt(b2.length()-1-i) == '1') {
+
+            }
+        }
+        System.out.print();
     }
 }
