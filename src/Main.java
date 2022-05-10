d4cc9fa25f2e1b55fb8ccdc3d44f17243c642f29
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-10 10:23:24 +0900

    :tada: solve baekjoon-2774

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,22 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String str = bufferedReader.readLine();
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
-                str = str.substring(0, i) + str.substring(i + 2);
+        int T = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            int[] array = new int[10];
+            String num = bufferedReader.readLine();
+            for (int j = 0; j < num.length(); j++) {
+                array[num.charAt(j) - '0']++;
             }
+            int count = 0;
+            for (int j = 0; j < 10; j++) {
+                if (array[j] != 0) {
+                    count++;
+                }
+            }
+            stringBuilder.append(count).append("\n");
         }
-        System.out.print(str);
+        System.out.print(stringBuilder);
     }
 }
