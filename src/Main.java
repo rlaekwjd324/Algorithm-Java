15ac36ce1a5119839e0453e1728988649599f20b
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-11 11:59:43 +0900

    :tada: solve baekjoon-3059

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,16 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(bufferedReader.readLine());
+        int t = Integer.parseInt(bufferedReader.readLine());
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < T; i++) {
-            int[] array = new int[10];
-            String num = bufferedReader.readLine();
-            for (int j = 0; j < num.length(); j++) {
-                array[num.charAt(j) - '0']++;
-            }
-            int count = 0;
-            for (int j = 0; j < 10; j++) {
-                if (array[j] != 0) {
-                    count++;
+        for (int i = 0; i < t; i++) {
+            int count = 2015;
+            boolean[] array = new boolean[26];
+            String str = bufferedReader.readLine();
+            for (int j = 0; j < str.length(); j++) {
+                if (!array[str.charAt(j) - 65]) {
+                    array[str.charAt(j) - 65] = true;
+                    count -= str.charAt(j);
                 }
             }
             stringBuilder.append(count).append("\n");
