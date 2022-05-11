12cd5d31518e70086f38824e12a72f1ec0b90eb0
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-11 11:59:08 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            int count = 2015;
+            boolean[] array = new boolean[26];
+            String str = bufferedReader.readLine();
+            for (int j = 0; j < str.length(); j++) {
+                if (!array[str.charAt(j) - 65]) {
+                    array[str.charAt(j) - 65] = true;
+                    count -= str.charAt(j);
+                }
+            }
+            stringBuilder.append(count).append("\n");
+        }
+        System.out.print(stringBuilder);
     }
 }
