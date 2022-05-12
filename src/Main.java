e2840f240e6369f1ac4e2ff09f5775b558429bb4
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-12 14:54:07 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int M = Integer.parseInt(stringTokenizer.nextToken());
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int maxH = 0;
+        for (int i = 0; i < N; i++) {
+            maxH = Math.max(maxH, Integer.parseInt(stringTokenizer.nextToken()));
+        }
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int maxA = 0;
+        for (int i = 0; i < M; i++) {
+            maxA = Math.max(maxA, Integer.parseInt(stringTokenizer.nextToken()));
+        }
+        System.out.print(maxH + maxA);
     }
 }
