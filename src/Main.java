dbf25957f0372304963571c46f8798d2636492f1
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-12 14:55:41 +0900

    :tada: solve baekjoon-15781

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(bufferedReader.readLine());
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < t; i++) {
-            int count = 2015;
-            boolean[] array = new boolean[26];
-            String str = bufferedReader.readLine();
-            for (int j = 0; j < str.length(); j++) {
-                if (!array[str.charAt(j) - 65]) {
-                    array[str.charAt(j) - 65] = true;
-                    count -= str.charAt(j);
-                }
-            }
-            stringBuilder.append(count).append("\n");
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int M = Integer.parseInt(stringTokenizer.nextToken());
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int maxH = 0;
+        for (int i = 0; i < N; i++) {
+            maxH = Math.max(maxH, Integer.parseInt(stringTokenizer.nextToken()));
         }
-        System.out.print(stringBuilder);
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int maxA = 0;
+        for (int i = 0; i < M; i++) {
+            maxA = Math.max(maxA, Integer.parseInt(stringTokenizer.nextToken()));
+        }
+        System.out.print(maxH + maxA);
     }
 }
