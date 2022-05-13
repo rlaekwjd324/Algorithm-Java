08e708ee8c71d64da92b1e9d7d4fdbfeb8a5989e
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-13 13:19:08 +0900

    :tada: solve baekjoon-11549

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,14 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(bufferedReader.readLine());
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int N = Integer.parseInt(stringTokenizer.nextToken());
-        int M = Integer.parseInt(stringTokenizer.nextToken());
-        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int maxH = 0;
-        for (int i = 0; i < N; i++) {
-            maxH = Math.max(maxH, Integer.parseInt(stringTokenizer.nextToken()));
+        int count = 0;
+        for (int i = 0; i < 5; i++) {
+            if (Integer.parseInt(stringTokenizer.nextToken()) == t) {
+                count++;
+            }
         }
-        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int maxA = 0;
-        for (int i = 0; i < M; i++) {
-            maxA = Math.max(maxA, Integer.parseInt(stringTokenizer.nextToken()));
-        }
-        System.out.print(maxH + maxA);
+        System.out.println(count);
     }
 }
