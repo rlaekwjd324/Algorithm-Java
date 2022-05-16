ce222a9bba798634478c74615d6b77f78e58deb1
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-16 11:59:38 +0900

    :tada: solve baekjoon-17389

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,19 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bufferedReader.readLine())*3;
-        n += Integer.parseInt(bufferedReader.readLine())*2;
-        n += Integer.parseInt(bufferedReader.readLine())*1;
-        int m = Integer.parseInt(bufferedReader.readLine())*3;
-        m += Integer.parseInt(bufferedReader.readLine())*2;
-        m += Integer.parseInt(bufferedReader.readLine())*1;
-        if(n>m ){
-            System.out.print("A");
-        }else if(n<m) {
-            System.out.print("B");
-        }else {
-            System.out.print("T");
+        int n = Integer.parseInt(bufferedReader.readLine());
+        String str = bufferedReader.readLine();
+        int sum = 0;
+        int bonus = 0;
+        for (int i = 1; i <= n; i++) {
+            if (str.charAt(i - 1) == 'O') {
+                sum += i;
+                sum += bonus;
+                bonus++;
+            } else {
+                bonus = 0;
+            }
         }
+        System.out.print(sum);
     }
 }
