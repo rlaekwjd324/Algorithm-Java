00cc6c0fb8531153d9fe8287d40d6287ec2711b0
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-16 11:58:58 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        String str = bufferedReader.readLine();
+        int sum = 0;
+        int bonus = 0;
+        for (int i = 1; i <= n; i++) {
+            if (str.charAt(i - 1) == 'O') {
+                sum += i;
+                sum += bonus;
+                bonus++;
+            } else {
+                bonus = 0;
+            }
+        }
+        System.out.print(sum);
     }
 }
