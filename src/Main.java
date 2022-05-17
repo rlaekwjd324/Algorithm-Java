d0654c50bf8fb695ca0c4ccb0fa0b83154e4af1b
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-17 10:23:24 +0900

    :tada: solve baekjoon-17173

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bufferedReader.readLine());
-        String str = bufferedReader.readLine();
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int n = Integer.parseInt(stringTokenizer.nextToken());
+        int m = Integer.parseInt(stringTokenizer.nextToken());
+        int[] array = new int[m];
         int sum = 0;
-        int bonus = 0;
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 0; i < m; i++) {
+            array[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
         for (int i = 1; i <= n; i++) {
-            if (str.charAt(i - 1) == 'O') {
-                sum += i;
-                sum += bonus;
-                bonus++;
-            } else {
-                bonus = 0;
+            for (int j = 0; j < m; j++) {
+                if (i % array[j] == 0) {
+                    sum += i;
+                    break;
+                }
             }
         }
         System.out.print(sum);
