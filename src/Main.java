66a4396b79a69863921cb4374b169823063866bf
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-17 10:22:48 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int n = Integer.parseInt(stringTokenizer.nextToken());
+        int m = Integer.parseInt(stringTokenizer.nextToken());
+        int[] array = new int[m];
+        int sum = 0;
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 0; i < m; i++) {
+            array[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i % array[j] == 0) {
+                    sum += i;
+                    break;
+                }
+            }
+        }
+        System.out.print(sum);
     }
 }
