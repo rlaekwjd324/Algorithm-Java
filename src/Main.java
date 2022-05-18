39bb659cc97a388640dd28f61861f68c6ee1a481
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-18 12:55:22 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        int count = 0;
+        for (int i = 1; i <= N; i++) {
+            int sum = 0;
+            for (int j = 0; j < String.valueOf(i).length(); j++) {
+                sum += (String.valueOf(i).charAt(j) - '0');
+            }
+            if (i % sum == 0) {
+                count++;
+            }
+        }
+        System.out.print(count);
     }
 }
