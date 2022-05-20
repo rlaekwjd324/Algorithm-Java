ea6a6b48355685a9892825ca07e9e61ee54bbc36
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-20 13:10:59 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String str = bufferedReader.readLine();
+        if (str.startsWith("555")) {
+            System.out.print("YES");
+            return;
+        }
+        System.out.print("NO");
     }
 }
