7595cf2d3fc1da67567423606f77382b16c35d00
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-20 13:11:17 +0900

    :tada: solve baekjoon-17863

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,11 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(bufferedReader.readLine());
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < t; i++) {
-            int d = Integer.parseInt(bufferedReader.readLine());
-            boolean isExist = false;
-            for (int j = d / 2; j > 0; j--) {
-                if (j * (j + 1) <= d) {
-                    stringBuilder.append(j).append("\n");
-                    isExist = true;
-                    break;
-                }
-            }
-            if (!isExist) {
-                stringBuilder.append("0").append("\n");
-            }
+        String str = bufferedReader.readLine();
+        if (str.startsWith("555")) {
+            System.out.print("YES");
+            return;
         }
-        System.out.print(stringBuilder);
+        System.out.print("NO");
     }
 }
