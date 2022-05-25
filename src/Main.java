147dfea7798605da6b9598f2caaaec72237efbc4
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-25 13:47:19 +0900

    :tada: solve baekjoon-14909

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,14 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(bufferedReader.readLine());
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < t; i++) {
-            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int p = Integer.parseInt(stringTokenizer.nextToken());
-            int m = Integer.parseInt(stringTokenizer.nextToken());
-            int count = 0;
-            boolean[] array = new boolean[m];
-            for (int j = 0; j < p; j++) {
-                int position = Integer.parseInt(bufferedReader.readLine());
-                if (array[position - 1]) {
-                    count++;
-                } else {
-                    array[position - 1] = true;
-                }
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int count = 0;
+        while (stringTokenizer.hasMoreTokens()) {
+            int n = Integer.parseInt(stringTokenizer.nextToken());
+            if (n > 0) {
+                count++;
             }
-            stringBuilder.append(count).append("\n");
         }
-        System.out.print(stringBuilder);
+        System.out.print(count);
     }
 }
