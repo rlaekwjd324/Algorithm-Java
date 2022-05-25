2498cdc74de648fd1be82b1983c6ecc5d27ed109
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-25 13:42:56 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int count = 0;
+        while (stringTokenizer.hasMoreTokens()) {
+            int n = Integer.parseInt(stringTokenizer.nextToken());
+            if (n > 0) {
+                count++;
+            }
+        }
+        System.out.print(count);
     }
 }
