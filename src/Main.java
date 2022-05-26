efbfbec0e496881a7c31cbfb10c4801cf8226abb
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-26 11:02:52 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n1 = Integer.parseInt(bufferedReader.readLine());
+        if (n1 != 8 && n1 != 9) {
+            System.out.print("answer");
+            return;
+        }
+        int n2 = Integer.parseInt(bufferedReader.readLine());
+        int n3 = Integer.parseInt(bufferedReader.readLine());
+        if (n2 != n3) {
+            System.out.print("answer");
+            return;
+        }
+        int n4 = Integer.parseInt(bufferedReader.readLine());
+        if (n4 != 8 && n4 != 9) {
+            System.out.print("answer");
+            return;
+        }
+        System.out.print("ignore");
     }
 }
