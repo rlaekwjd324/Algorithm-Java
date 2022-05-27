4fff8efd3dacd60456264934a147498435c2aa1d
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-27 12:50:00 +0900

    :tada: solve baekjoon-13597

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n1 = Integer.parseInt(bufferedReader.readLine());
-        if (n1 != 8 && n1 != 9) {
-            System.out.print("answer");
-            return;
-        }
-        int n2 = Integer.parseInt(bufferedReader.readLine());
-        int n3 = Integer.parseInt(bufferedReader.readLine());
-        if (n2 != n3) {
-            System.out.print("answer");
-            return;
-        }
-        int n4 = Integer.parseInt(bufferedReader.readLine());
-        if (n4 != 8 && n4 != 9) {
-            System.out.print("answer");
-            return;
-        }
-        System.out.print("ignore");
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int a = Integer.parseInt(stringTokenizer.nextToken());
+        int b = Integer.parseInt(stringTokenizer.nextToken());
+        System.out.print(Math.max(a, b));
     }
 }
