3d51ff6464e24fb8e1f4c7e735295b0a33eb8da7
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-30 12:57:20 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int L = Integer.parseInt(bufferedReader.readLine());
+        String str = bufferedReader.readLine();
+        int sum = 0;
+        for (int i = 0; i < L; i++) {
+            sum += (str.charAt(i) - 64);
+        }
+        System.out.print(sum);
     }
 }
