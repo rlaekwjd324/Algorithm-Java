055f0702d5795d5173f5a293c7af46662a34c920
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-30 12:58:01 +0900

    :tada: solve baekjoon-15813

--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,12 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int L = Integer.parseInt(bufferedReader.readLine());
         String str = bufferedReader.readLine();
-        StringBuilder stringBuilder = new StringBuilder();
-        while (!str.equals("#")) {
-            boolean[] array = new boolean[26];
-            int count = 0;
-            for (int i = 0; i < str.length(); i++) {
-                if (str.charAt(i) >= 65 && str.charAt(i) <= 90) {
-                    array[str.charAt(i) - 65] = true;
-                } else if (str.charAt(i) >= 97 && str.charAt(i) <= 122) {
-                    array[str.charAt(i) - 97] = true;
-                }
-            }
-            for (int i = 0; i < array.length; i++) {
-                if (array[i]) {
-                    count++;
-                }
-            }
-            stringBuilder.append(count).append("\n");
-            str = bufferedReader.readLine();
+        int sum = 0;
+        for (int i = 0; i < L; i++) {
+            sum += (str.charAt(i) - 64);
         }
-        System.out.print(stringBuilder);
+        System.out.print(sum);
     }
 }
