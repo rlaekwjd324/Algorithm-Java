125f832b4d2e7b41bc943d2cf640ab7b7a87a9a9
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-05-31 09:12:51 +0900

    :tada: solve baekjoon-4892

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,26 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int L = Integer.parseInt(bufferedReader.readLine());
-        String str = bufferedReader.readLine();
-        int sum = 0;
-        for (int i = 0; i < L; i++) {
-            sum += (str.charAt(i) - 64);
+        StringBuilder stringBuilder = new StringBuilder();
+        int n = Integer.parseInt(bufferedReader.readLine());
+        int count = 1;
+        while (n != 0) {
+            int n1 = n * 3;
+            int n2;
+            String str = "";
+            if (n1 % 2 == 0) {
+                n2 = n1 / 2;
+                str = "even";
+            } else {
+                n2 = (n1 + 1) / 2;
+                str = "odd";
+            }
+            int n3 = 3 * n2;
+            int n4 = n3 / 9;
+            stringBuilder.append(count).append(". ").append(str).append(" ").append(n4).append("\n");
+            count++;
+            n = Integer.parseInt(bufferedReader.readLine());
         }
-        System.out.print(sum);
+        System.out.print(stringBuilder);
     }
 }
