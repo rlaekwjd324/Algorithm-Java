c016215106d7de168cb617ae5b7f16a59b6c14a7
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-06-02 17:11:51 +0900

    :tada: solve baekjoon-6778

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bufferedReader.readLine());
+        int a = Integer.parseInt(bufferedReader.readLine());
+        int e = Integer.parseInt(bufferedReader.readLine());
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int x = Integer.parseInt(stringTokenizer.nextToken());
-            int y = Integer.parseInt(stringTokenizer.nextToken());
-            if (x < y) {
-                stringBuilder.append("NO BRAINS").append("\n");
-            } else {
-                stringBuilder.append("MMM BRAINS").append("\n");
-            }
+        if (a >= 3 && e <= 4) {
+            stringBuilder.append("TroyMartian").append("\n");
+        }
+        if (a <= 6 && e >= 2) {
+            stringBuilder.append("VladSaturnian").append("\n");
+        }
+        if (a <= 2 && e <= 3) {
+            stringBuilder.append("GraemeMercurian").append("\n");
         }
         System.out.print(stringBuilder);
     }
