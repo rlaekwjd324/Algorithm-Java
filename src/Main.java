9fae80c021f3bae2635b96caf334e8e1110eea63
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-06-03 17:53:25 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int c = 0;
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(stringTokenizer.nextToken());
+            if (num == c % 3) {
+                c++;
+            }
+        }
+        System.out.print(c);
     }
 }
