313191011aa3fd65784af8e20c9e7719684e5f6b
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-06-03 17:53:21 +0900

    :tada: solve baekjoon-14720

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(bufferedReader.readLine());
-        int e = Integer.parseInt(bufferedReader.readLine());
-        StringBuilder stringBuilder = new StringBuilder();
-        if (a >= 3 && e <= 4) {
-            stringBuilder.append("TroyMartian").append("\n");
+        int n = Integer.parseInt(bufferedReader.readLine());
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int c = 0;
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(stringTokenizer.nextToken());
+            if (num == c % 3) {
+                c++;
+            }
         }
-        if (a <= 6 && e >= 2) {
-            stringBuilder.append("VladSaturnian").append("\n");
-        }
-        if (a <= 2 && e <= 3) {
-            stringBuilder.append("GraemeMercurian").append("\n");
-        }
-        System.out.print(stringBuilder);
+        System.out.print(c);
     }
 }
