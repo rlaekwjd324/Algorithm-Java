46037335335136060bb8e621682b3eb8e2875c31
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-06-04 15:16:50 +0900

    :tada: solve baekjoon-11091

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bufferedReader.readLine());
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int c = 0;
+        StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(stringTokenizer.nextToken());
-            if (num == c % 3) {
-                c++;
+            String str = bufferedReader.readLine().trim();
+            boolean[] isExist = new boolean[26];
+            for (int j = 0; j < str.length(); j++) {
+                char c = str.charAt(j);
+                if (c >= 97 && c <= 122) {
+                    isExist[c - 97] = true;
+                } else if (c >= 65 && c <= 90) {
+                    isExist[c - 65] = true;
+                }
+            }
+            String s = "";
+            for (int j = 0; j < isExist.length; j++) {
+                if (!isExist[j]) {
+                    s += (char) (j + 97);
+                }
+            }
+            if (s.length() == 0) {
+                stringBuilder.append("pangram").append("\n");
+            } else {
+                stringBuilder.append("missing ").append(s).append("\n");
             }
         }
-        System.out.print(c);
+        System.out.print(stringBuilder);
     }
 }
