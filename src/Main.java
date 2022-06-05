9afb9d37e49e20c735b7262acff9725b7e4d1706
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-06-05 14:28:47 +0900

    :tada: solve baekjoon-10599

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bufferedReader.readLine());
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            String str = bufferedReader.readLine().trim();
-            boolean[] isExist = new boolean[26];
-            for (int j = 0; j < str.length(); j++) {
-                char c = str.charAt(j);
-                if (c >= 97 && c <= 122) {
-                    isExist[c - 97] = true;
-                } else if (c >= 65 && c <= 90) {
-                    isExist[c - 65] = true;
-                }
-            }
-            String s = "";
-            for (int j = 0; j < isExist.length; j++) {
-                if (!isExist[j]) {
-                    s += (char) (j + 97);
-                }
-            }
-            if (s.length() == 0) {
-                stringBuilder.append("pangram").append("\n");
-            } else {
-                stringBuilder.append("missing ").append(s).append("\n");
-            }
+        int n1 = Integer.parseInt(stringTokenizer.nextToken());
+        int n2 = Integer.parseInt(stringTokenizer.nextToken());
+        int n3 = Integer.parseInt(stringTokenizer.nextToken());
+        int n4 = Integer.parseInt(stringTokenizer.nextToken());
+        while (n1 != 0 || n2 != 0 || n3 != 0 || n4 != 0) {
+            stringBuilder.append(n3 - n2).append(" ").append(n4 - n1).append("\n");
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            n1 = Integer.parseInt(stringTokenizer.nextToken());
+            n2 = Integer.parseInt(stringTokenizer.nextToken());
+            n3 = Integer.parseInt(stringTokenizer.nextToken());
+            n4 = Integer.parseInt(stringTokenizer.nextToken());
         }
         System.out.print(stringBuilder);
     }
