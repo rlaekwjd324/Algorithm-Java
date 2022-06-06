5083e55a0eb8cad1079457d7048e2c287d9cb4db
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-06-07 08:50:33 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] array = new String[N];
+        for (int i = 0; i < N; i++) {
+            array[i] = bufferedReader.readLine();
+        }
+        int K = Integer.parseInt(bufferedReader.readLine());
+        switch (K) {
+            case 1:
+                for (int i = 0; i < N; i++) {
+                    stringBuilder.append(array[i]).append("\n");
+                }
+                break;
+            case 2:
+                for (int i = 0; i < N; i++) {
+                    StringBuilder sb = new StringBuilder(array[i]);
+                    stringBuilder.append(sb.reverse()).append("\n");
+                }
+                break;
+            case 3:
+                for (int i = 0; i < N; i++) {
+                    stringBuilder.append(array[N - i - 1]).append("\n");
+                }
+                break;
+        }
+        System.out.print(stringBuilder);
     }
 }
