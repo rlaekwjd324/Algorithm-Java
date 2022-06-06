a9feb264679db55da68adc48fdf54c731ec2af1c
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-06-06 13:22:45 +0900

    :tada: solve baekjoon-14489

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        StringBuilder stringBuilder = new StringBuilder();
-        int n1 = Integer.parseInt(stringTokenizer.nextToken());
-        int n2 = Integer.parseInt(stringTokenizer.nextToken());
-        int n3 = Integer.parseInt(stringTokenizer.nextToken());
-        int n4 = Integer.parseInt(stringTokenizer.nextToken());
-        while (n1 != 0 || n2 != 0 || n3 != 0 || n4 != 0) {
-            stringBuilder.append(n3 - n2).append(" ").append(n4 - n1).append("\n");
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            n1 = Integer.parseInt(stringTokenizer.nextToken());
-            n2 = Integer.parseInt(stringTokenizer.nextToken());
-            n3 = Integer.parseInt(stringTokenizer.nextToken());
-            n4 = Integer.parseInt(stringTokenizer.nextToken());
+        long a = Long.parseLong(stringTokenizer.nextToken());
+        long b = Long.parseLong(stringTokenizer.nextToken());
+        long c = Long.parseLong(bufferedReader.readLine());
+        if (a + b < c * 2) {
+            System.out.print(a + b);
+        } else {
+            System.out.println(a + b - c * 2);
         }
-        System.out.print(stringBuilder);
     }
 }
