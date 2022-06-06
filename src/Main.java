47d4cf116a470536f86e7f4eb959e28ca54c1e47
KimDaJung <ekwjd324@gmail.com>
2022-06-06 13:22:02 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        long a = Long.parseLong(stringTokenizer.nextToken());
+        long b = Long.parseLong(stringTokenizer.nextToken());
+        long c = Long.parseLong(bufferedReader.readLine());
+        if (a + b < c * 2) {
+            System.out.print(a + b);
+        } else {
+            System.out.println(a + b - c * 2);
+        }
     }
 }
