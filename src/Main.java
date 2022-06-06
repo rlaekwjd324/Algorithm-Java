4a7638f6dcb6f2b64dd39e29cfee8be47f21be08
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-06-07 08:50:59 +0900

    :tada: solve baekjoon-11586

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        long a = Long.parseLong(stringTokenizer.nextToken());
-        long b = Long.parseLong(stringTokenizer.nextToken());
-        long c = Long.parseLong(bufferedReader.readLine());
-        if (a + b < c * 2) {
-            System.out.print(a + b);
-        } else {
-            System.out.println(a + b - c * 2);
+        int N = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] array = new String[N];
+        for (int i = 0; i < N; i++) {
+            array[i] = bufferedReader.readLine();
         }
+        int K = Integer.parseInt(bufferedReader.readLine());
+        switch (K) {
+            case 1:
+                for (int i = 0; i < N; i++) {
+                    stringBuilder.append(array[i]).append("\n");
+                }
+                break;
+            case 2:
+                for (int i = 0; i < N; i++) {
+                    StringBuilder sb = new StringBuilder(array[i]);
+                    stringBuilder.append(sb.reverse()).append("\n");
+                }
+                break;
+            case 3:
+                for (int i = 0; i < N; i++) {
+                    stringBuilder.append(array[N - i - 1]).append("\n");
+                }
+                break;
+        }
+        System.out.print(stringBuilder);
     }
 }
