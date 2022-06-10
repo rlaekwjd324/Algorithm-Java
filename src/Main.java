ef312467af2fcd176d753002e275b7beb1fea113
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-06-10 16:15:42 +0900

    :tada: solve baekjoon-23037

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int n = Integer.parseInt(stringTokenizer.nextToken());
-        StringBuilder stringBuilder = new StringBuilder();
-        while (n != 0) {
-            int preNumber = 0;
-            for (int i = 0; i < n; i++) {
-                int num = Integer.parseInt(stringTokenizer.nextToken());
-                if (preNumber != num) {
-                    preNumber = num;
-                    stringBuilder.append(preNumber).append(" ");
-                }
-            }
-            stringBuilder.append("$").append("\n");
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            n = Integer.parseInt(stringTokenizer.nextToken());
+        String n = bufferedReader.readLine();
+        int num = 0;
+        for (int i = 0; i < n.length(); i++) {
+            num += Math.pow((n.charAt(i) - '0'), 5);
         }
-        System.out.print(stringBuilder);
+        System.out.print(num);
     }
 }
