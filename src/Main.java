ab622dc32d8d2e7ee303991a4c988233fb8a0493
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-06-12 13:22:25 +0900

    :tada: solve baekjoon-2744

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(bufferedReader.readLine());
+        String str = bufferedReader.readLine();
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < t; i++) {
-            int n = Integer.parseInt(bufferedReader.readLine());
-            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int[] array = new int[n];
-            for (int j = 0; j < n; j++) {
-                array[j] = Integer.parseInt(stringTokenizer.nextToken());
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c >= 97) {
+                stringBuilder.append((char) (c - 32));
+            } else {
+                stringBuilder.append((char) (c + 32));
             }
-            Arrays.sort(array);
-            stringBuilder.append(array[0]).append(" ").append(array[n - 1]).append("\n");
         }
         System.out.print(stringBuilder);
     }
