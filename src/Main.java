b0685057b6386bcac6ce5840292eb5cefe8ed9ad
KimDaJung <ekwjd324@gmail.com>
2022-06-13 18:23:22 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n1 = Integer.parseInt(bufferedReader.readLine());
+        int n2 = Integer.parseInt(bufferedReader.readLine());
+        if (n2 - n1 < 1) {
+            System.out.print("Congratulations, you are within the speed limit!");
+        } else if (n2 - n1 < 21) {
+            System.out.print("You are speeding and your fine is $100.");
+        } else if (n2 - n1 < 31) {
+            System.out.print("You are speeding and your fine is $270.");
+        } else {
+            System.out.print("You are speeding and your fine is $500.");
+        }
     }
 }
