b13c1f0e82ea75ca5c9b305bb25ed35c3afaf11f
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-06-14 09:02:35 +0900

    :tada: solve baekjoon-5789

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,16 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n1 = Integer.parseInt(bufferedReader.readLine());
-        int n2 = Integer.parseInt(bufferedReader.readLine());
-        if (n2 - n1 < 1) {
-            System.out.print("Congratulations, you are within the speed limit!");
-            return;
-        } else if (n2 - n1 < 21) {
-            System.out.print("You are speeding and your fine is $100.");
-            return;
-        } else if (n2 - n1 < 31) {
-            System.out.print("You are speeding and your fine is $270.");
-            return;
-        } else {
-            System.out.print("You are speeding and your fine is $500.");
+        int n = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            String str = bufferedReader.readLine();
+            if (str.charAt(str.length() / 2) == str.charAt(str.length() / 2 - 1)) {
+                stringBuilder.append("Do-it").append("\n");
+            } else {
+                stringBuilder.append("Do-it-Not").append("\n");
+            }
         }
+        System.out.print(stringBuilder);
     }
 }
