4701ec9a702c423cacc4d612ea4c72375bb2d0c8
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-06-14 09:02:39 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            String str = bufferedReader.readLine();
+            if (str.charAt(str.length() / 2) == str.charAt(str.length() / 2 - 1)) {
+                stringBuilder.append("Do-it").append("\n");
+            } else {
+                stringBuilder.append("Do-it-Not").append("\n");
+            }
+        }
+        System.out.print(stringBuilder);
     }
 }
