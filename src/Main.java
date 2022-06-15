444f6c9bf17258473117c926f8088798488c874f
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-06-15 14:33:12 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String str = bufferedReader.readLine();
+        StringBuilder stringBuilder = new StringBuilder();
+        while (!str.equals("#")) {
+            int count = 0;
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == '1' || str.charAt(i) == 'o') {
+                    count++;
+                }
+            }
+            str = str.substring(0, str.length() - 1);
+            if (count % 2 == 1) {
+                str += "1";
+            } else {
+                str += "0";
+            }
+            stringBuilder.append(str).append("\n");
+            str = bufferedReader.readLine();
+        }
+        System.out.print(stringBuilder);
     }
 }
