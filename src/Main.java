0bd389d87a8817b3c60494fb594b5573a7dc9fda
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-06-15 14:34:30 +0900

    :tada: solve baekjoon-4597

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,23 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bufferedReader.readLine());
+        String str = bufferedReader.readLine();
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            String str = bufferedReader.readLine();
-            if (str.charAt(str.length() / 2) == str.charAt(str.length() / 2 - 1)) {
-                stringBuilder.append("Do-it").append("\n");
-            } else {
-                stringBuilder.append("Do-it-Not").append("\n");
+        while (!str.equals("#")) {
+            int count = 0;
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == '1' || str.charAt(i) == 'o') {
+                    count++;
+                }
             }
+            str = str.substring(0, str.length() - 1);
+            if (count % 2 == 1) {
+                str += "1";
+            } else {
+                str += "0";
+            }
+            stringBuilder.append(str).append("\n");
+            str = bufferedReader.readLine();
         }
         System.out.print(stringBuilder);
     }
