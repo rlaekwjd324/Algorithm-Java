3d3dcb816a845ed3e1ce45a80111bf251b77ef82
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-06-16 14:07:21 +0900

    :tada: solve baekjoon-12780

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,14 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String str = bufferedReader.readLine();
-        StringBuilder stringBuilder = new StringBuilder();
-        while (!str.equals("#")) {
-            int count = 0;
-            for (int i = 0; i < str.length(); i++) {
-                if (str.charAt(i) == '1' || str.charAt(i) == 'o') {
-                    count++;
-                }
-            }
-            str = str.substring(0, str.length() - 1);
-            if (count % 2 == 1) {
-                str += "1";
-            } else {
-                str += "0";
-            }
-            stringBuilder.append(str).append("\n");
-            str = bufferedReader.readLine();
+        String H = bufferedReader.readLine();
+        String N = bufferedReader.readLine();
+        int index = H.indexOf(N);
+        int count = 0;
+        while (index != -1) {
+            count++;
+            index = H.indexOf(N, index + 1);
         }
-        System.out.print(stringBuilder);
+        System.out.print(count);
     }
 }
