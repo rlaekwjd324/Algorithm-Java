1b68ca3ee10ab8d3ff3ff368c16dc6bb6b502383
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-06-21 08:45:16 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String mbti = bufferedReader.readLine();
+        StringBuilder stringBuilder = new StringBuilder();
+        char[] array = {'E', 'I', 'S', 'N', 'F', 'T', 'P', 'J'};
+        for (int i = 0; i < mbti.length(); i++) {
+            stringBuilder.append(mbti.charAt(i) == array[2 * i] ? array[2 * i + 1] : array[2 * i]);
+        }
+        System.out.print(stringBuilder);
     }
 }
