f1432c08c1603df095bd4228c0cf8596b67dbf57
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-06-21 08:45:12 +0900

    :tada: solve baekjoon-20540

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        String[] array = new String[50];
-        for (int i = 0; i < 50; i++) {
-            array[i] = stringTokenizer.nextToken();
+        String mbti = bufferedReader.readLine();
+        StringBuilder stringBuilder = new StringBuilder();
+        char[] array = {'E', 'I', 'S', 'N', 'F', 'T', 'P', 'J'};
+        for (int i = 0; i < mbti.length(); i++) {
+            stringBuilder.append(mbti.charAt(i) == array[2 * i] ? array[2 * i + 1] : array[2 * i]);
         }
-        String str = bufferedReader.readLine();
-        int index = Arrays.asList(array).indexOf(str);
-        if (index < 5) {
-            System.out.print("A+");
-            return;
-        }
-        if (index < 15) {
-            System.out.print("A0");
-            return;
-        }
-        if (index < 30) {
-            System.out.print("B+");
-            return;
-        }
-        if (index < 35) {
-            System.out.print("B0");
-            return;
-        }
-        if (index < 45) {
-            System.out.print("C+");
-            return;
-        }
-        if (index < 48) {
-            System.out.print("C0");
-            return;
-        }
-        System.out.print("F");
+        System.out.print(stringBuilder);
     }
 }
