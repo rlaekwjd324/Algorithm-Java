e1cd21f13cd60e05958ad648b7b535c25d4ca01a
KimDaJung <ekwjd324@gmail.com>
2022-06-23 12:20:07 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int first = Integer.parseInt(bufferedReader.readLine());
+        int second = Integer.parseInt(bufferedReader.readLine());
+        System.out.print(second + second - first);
     }
 }
