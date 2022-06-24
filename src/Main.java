dac09e0c061e0edac3e73f8fe5edb7cc55415e4f
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-06-24 13:45:09 +0900

    :tada: solve baekjoon-10995

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,18 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int first = Integer.parseInt(bufferedReader.readLine());
-        int second = Integer.parseInt(bufferedReader.readLine());
-        System.out.print(second + second - first);
+        int N = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i % 2 == 0) {
+                    stringBuilder.append("*").append(" ");
+                } else {
+                    stringBuilder.append(" ").append("*");
+                }
+            }
+            stringBuilder.append("\n");
+        }
+        System.out.print(stringBuilder);
     }
 }
