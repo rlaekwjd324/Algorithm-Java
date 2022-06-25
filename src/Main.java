c176d79683700923e200be07b1738863e0ea6a32
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-06-25 13:24:34 +0900

    :tada: solve baekjoon-15000

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,10 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bufferedReader.readLine());
+        String str = bufferedReader.readLine();
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (i % 2 == 0) {
-                    stringBuilder.append("*").append(" ");
-                } else {
-                    stringBuilder.append(" ").append("*");
-                }
-            }
-            stringBuilder.append("\n");
+        for (int i = 0; i < str.length(); i++) {
+            stringBuilder.append((char) (str.charAt(i) - 32));
         }
         System.out.print(stringBuilder);
     }
