c4eba0cd3846d8fe5df2b105264ec5905ffc9994
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-06-27 10:29:45 +0900

    :tada: solve baekjoon-4999

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        long A = Long.parseLong(stringTokenizer.nextToken());
-        long B = Long.parseLong(stringTokenizer.nextToken());
-        long C = Long.parseLong(stringTokenizer.nextToken());
-        System.out.print(A + B + C);
+        String jawhanAh = bufferedReader.readLine();
+        String doctorAh = bufferedReader.readLine();
+        System.out.print(jawhanAh.length() >= doctorAh.length() ? "go" : "no");
     }
 }
