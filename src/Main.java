eed29d600f0991198ef69ad54e41dda98597cc96
KimDaJung <ekwjd324@gmail.com>
2022-06-27 10:29:06 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String jawhanAh = bufferedReader.readLine();
+        String doctorAh = bufferedReader.readLine();
+        System.out.print(jawhanAh.length() >= doctorAh.length() ? "go" : "no");
     }
 }
