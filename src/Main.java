7d8aaa8f755c5eef7cfda8d872a943788e39854c
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-06-28 11:45:08 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int L = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < L; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int n = Integer.parseInt(stringTokenizer.nextToken());
+            String str = stringTokenizer.nextToken();
+            stringBuilder.append(String.valueOf(str).repeat(Math.max(0, n)));
+            stringBuilder.append("\n");
+        }
+        System.out.print(stringBuilder);
     }
 }
