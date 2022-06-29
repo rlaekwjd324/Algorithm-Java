8b2836c7092c7e8c29d3378374a07192b8f1fb26
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-06-29 11:49:18 +0900

    :tada: solve baekjoon-17263

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int L = Integer.parseInt(bufferedReader.readLine());
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < L; i++) {
-            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int n = Integer.parseInt(stringTokenizer.nextToken());
-            String str = stringTokenizer.nextToken();
-            for (int j = 0; j < n; j++) {
-                stringBuilder.append(str);
-            }
-            stringBuilder.append("\n");
+        int N = Integer.parseInt(bufferedReader.readLine());
+        int[] array = new int[N];
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 0; i < N; i++) {
+            array[i] = Integer.parseInt(stringTokenizer.nextToken());
         }
-        System.out.print(stringBuilder);
+        Arrays.sort(array);
+        System.out.print(array[N - 1]);
     }
 }
