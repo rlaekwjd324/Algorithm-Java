47bb8bd2c49946ee2d93a9c57d8607fa21d5c653
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-07-01 10:24:38 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(bufferedReader.readLine());
-        StringBuilder str = new StringBuilder();
-        while (T >= 9) {
-            int d = T % 9;
-            T = T / 9;
-            str.insert(0, d);
-        }
-        System.out.print(T + str.toString());
+    public static void main(String[] args) {
+
     }
 }
