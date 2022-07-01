be560b0845d3053fee83efb46ce70993630adc35
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-07-01 10:23:45 +0900

    :tada: solve baekjoon-14491

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bufferedReader.readLine());
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            stringBuilder.append(Integer.parseInt(stringTokenizer.nextToken()) + Integer.parseInt(stringTokenizer.nextToken())).append("\n");
+        int T = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder str = new StringBuilder();
+        while (T >= 9) {
+            int d = T % 9;
+            T = T / 9;
+            str.insert(0, d);
         }
-        System.out.print(stringBuilder);
+        System.out.print(T + str.toString());
     }
 }
