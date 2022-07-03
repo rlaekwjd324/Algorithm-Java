520da2b2c505502666f346e43327ceac5c1bb575
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-07-03 17:53:15 +0900

    :tada: solve baekjoon-5363

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer stk = new StringTokenizer(br.readLine());
-	    int[] array = new int[3];
-		array[0] = Integer.parseInt(stk.nextToken());
-		array[1] = Integer.parseInt(stk.nextToken());
-		array[2] = Integer.parseInt(stk.nextToken());
-		Arrays.sort(array);
-		System.out.println(array[1]);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            String[] array = bufferedReader.readLine().split(" ");
+            String str = array[0] + " " + array[1];
+            for (int j = 2; j < array.length; j++) {
+                stringBuilder.append(array[j]).append(" ");
+            }
+            stringBuilder.append(str).append("\n");
+        }
+        System.out.print(stringBuilder);
+    }
 }
