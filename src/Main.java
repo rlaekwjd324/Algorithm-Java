a591dac478cd0e7eab335911b962b17cc91c3ce4
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-07-07 10:31:56 +0900

    :tada: solve baekjoon-25304

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int[] array = new int[3];
-        for (int i = 0; i < 3; i++) {
-            array[i] = Integer.parseInt(stringTokenizer.nextToken());
+        int pay = Integer.parseInt(bufferedReader.readLine());
+        int num = Integer.parseInt(bufferedReader.readLine());
+        int sum = 0;
+        for (int i = 0; i < num; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int tempPay = Integer.parseInt(stringTokenizer.nextToken());
+            int tempNum = Integer.parseInt(stringTokenizer.nextToken());
+            sum += tempPay * tempNum;
         }
-        Arrays.sort(array);
-        System.out.print(array[1]);
+        if (sum == pay) {
+            System.out.print("Yes");
+        } else {
+            System.out.print("No");
+        }
     }
 }
