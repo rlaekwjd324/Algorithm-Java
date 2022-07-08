994ce1ee6aa1b1fb04d95289f8dbc626f5647a13
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-07-08 12:35:38 +0900

    :tada: solve baekjoon-25314

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int pay = Integer.parseInt(bufferedReader.readLine());
-        int num = Integer.parseInt(bufferedReader.readLine());
-        int sum = 0;
-        for (int i = 0; i < num; i++) {
-            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int tempPay = Integer.parseInt(stringTokenizer.nextToken());
-            int tempNum = Integer.parseInt(stringTokenizer.nextToken());
-            sum += tempPay * tempNum;
+        int n = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < n / 4; i++) {
+            stringBuilder.append("long ");
         }
-        if (sum == pay) {
-            System.out.print("Yes");
-        } else {
-            System.out.print("No");
-        }
+        stringBuilder.append("int");
+        System.out.print(stringBuilder);
     }
 }
