8067e35eb2c8f398411596712abe0761283c4303
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-07-08 12:36:00 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("long ".repeat(Math.max(0, n / 4)));
+        stringBuilder.append("int");
+        System.out.print(stringBuilder);
     }
 }
