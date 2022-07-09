d1219ef9a196b016abc658f42b72a611116ba5af
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-07-09 14:23:29 +0900

    :tada: solve baekjoon-18408

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bufferedReader.readLine());
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < n / 4; i++) {
-            stringBuilder.append("long ");
-        }
-        stringBuilder.append("int");
-        System.out.print(stringBuilder);
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int[] array = {Integer.parseInt(stringTokenizer.nextToken()), Integer.parseInt(stringTokenizer.nextToken()), Integer.parseInt(stringTokenizer.nextToken())};
+        Arrays.sort(array);
+        System.out.print(array[1]);
     }
 }
