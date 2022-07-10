e3d31aad41fc1d8735363884dc295dbd57605ed2
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-07-10 15:49:26 +0900

    :tada: solve baekjoon-15734

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int[] array = {Integer.parseInt(stringTokenizer.nextToken()), Integer.parseInt(stringTokenizer.nextToken()), Integer.parseInt(stringTokenizer.nextToken())};
-        Arrays.sort(array);
-        System.out.print(array[1]);
+        int L = Integer.parseInt(stringTokenizer.nextToken());
+        int R = Integer.parseInt(stringTokenizer.nextToken());
+        int A = Integer.parseInt(stringTokenizer.nextToken());
+        while (A > 0) {
+            if (L > R) {
+                R++;
+            } else {
+                L++;
+            }
+            A--;
+        }
+        System.out.print(Math.min(L, R) * 2);
     }
 }
