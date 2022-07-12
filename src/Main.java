6632da5a79dee69ff2dc1365cd7148800efcb2cb
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-07-12 12:43:36 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int[] array = new int[3];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+        Arrays.sort(array);
+        System.out.print(array[1] + array[2]);
     }
 }
