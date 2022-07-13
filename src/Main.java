4107c6a23e8e37b03313a04dfd61a249cbd5f8c1
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-07-13 13:09:32 +0900

    :tada: solve baekjoon-23795

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int[] array = new int[3];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = Integer.parseInt(stringTokenizer.nextToken());
+        long n = Long.parseLong(bufferedReader.readLine());
+        long sum = 0;
+        while (n != -1) {
+            sum += n;
+            n = Integer.parseInt(bufferedReader.readLine());
         }
-        Arrays.sort(array);
-        System.out.print(array[1] + array[2]);
+        System.out.print(sum);
     }
 }
