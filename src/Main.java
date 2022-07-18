f7579aeefb79a6833aa1ca564b2ae712d39b1f1e
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-07-18 11:22:34 +0900

    :tada: solve baekjoon-9093

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String line = bufferedReader.readLine();
+        int T = Integer.parseInt(bufferedReader.readLine());
         StringBuilder answer = new StringBuilder();
-        String[] array = line.split("<");
-        if (array[0].length() != 0) {
-            answer.append(reverseString(array[0]));
-        }
-        for (int i = 1; i < array.length; i++) {
-            String[] temp = array[i].split(">");
-            String tag = temp[0];
-            answer.append("<").append(tag).append(">");
-            if (temp.length > 1) {
-                answer.append(reverseString(temp[1]));
+        for (int i = 0; i < T; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            while (stringTokenizer.hasMoreTokens()) {
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append(stringTokenizer.nextToken());
+                stringBuilder.reverse();
+                answer.append(stringBuilder).append(" ");
             }
+            answer.append("\n");
         }
         System.out.print(answer);
-    }
-
-    public static String reverseString(String str) {
-        StringBuilder stringBuilder;
-        StringBuilder answer = new StringBuilder();
-        String[] strArray = str.split(" ");
-        for (String temp : strArray) {
-            stringBuilder = new StringBuilder();
-            stringBuilder.append(temp);
-            stringBuilder.reverse();
-            answer.append(stringBuilder).append(" ");
-        }
-        answer.deleteCharAt(answer.length() - 1);
-        return answer.toString();
     }
 }
