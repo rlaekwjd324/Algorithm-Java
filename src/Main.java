68ca008ead62e2dc9d64d1dc2caf3d245f202716
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-07-21 16:16:54 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int x = reverseNumber(stringTokenizer.nextToken());
+        int y = reverseNumber(stringTokenizer.nextToken());
+        System.out.print(reverseNumber(String.valueOf(x + y)));
+    }
+
+    public static int reverseNumber(String str) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(str);
+        return Integer.parseInt(stringBuilder.reverse().toString());
     }
 }
