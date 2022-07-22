35da6de9b438a4273e7af9cdfcada3be8e1f0d54
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-07-22 10:37:41 +0900

    :tada: solve baekjoon-11816

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int x = reverseNumber(stringTokenizer.nextToken());
-        int y = reverseNumber(stringTokenizer.nextToken());
-        System.out.print(reverseNumber(String.valueOf(x + y)));
-    }
-
-    public static int reverseNumber(String str) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(str);
-        return Integer.parseInt(stringBuilder.reverse().toString());
+        String str = bufferedReader.readLine();
+        int answer = 0;
+        if (str.charAt(0) == '0') {
+//            16진수
+            if (str.charAt(1) == 'x') {
+                for (int i = str.length() - 1; i >= 2; i--) {
+                    char c = str.charAt(i);
+                    int num = 0;
+                    if (c >= 97) {
+                        num = c - 87;
+                    } else {
+                        num = c - '0';
+                    }
+                    answer += (num) * Math.pow(16, str.length() - i - 1);
+                }
+            }
+//            8진수
+            else {
+                for (int i = str.length() - 1; i >= 1; i--) {
+                    answer += (str.charAt(i) - '0') * Math.pow(8, str.length() - i - 1);
+                }
+            }
+        }
+//        10진수
+        else {
+            answer = Integer.parseInt(str);
+        }
+        System.out.print(answer);
     }
 }
