71b97f9d4d52a41a3d85e02a3c6eac33e4e85d00
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-07-23 17:50:14 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        ArrayList<Person> arrayList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            Person person = new Person(stringTokenizer.nextToken(), Integer.parseInt(stringTokenizer.nextToken()), Integer.parseInt(stringTokenizer.nextToken()), Integer.parseInt(stringTokenizer.nextToken()));
+            arrayList.add(person);
+        }
+        arrayList.sort((o1, o2) -> {
+            if (o1.year > o2.year) {
+                return 1;
+            } else if (o1.year == o2.year) {
+                if (o1.month > o2.month) {
+                    return 1;
+                } else if (o1.month == o2.month) {
+                    if (o1.day > o2.day) {
+                        return 1;
+                    }
+                } else {
+                    return -1;
+                }
+            } else {
+                return -1;
+            }
+            return 0;
+        });
+        System.out.println(arrayList.get(arrayList.size() - 1).name);
+        System.out.println(arrayList.get(0).name);
+    }
+
+    public static class Person {
+        String name;
+        int day;
+        int month;
+        int year;
+
+        public Person(String name, int day, int month, int year) {
+            this.name = name;
+            this.day = day;
+            this.month = month;
+            this.year = year;
+        }
     }
 }
