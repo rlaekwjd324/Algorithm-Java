e9b1d7d648736c202300e751c6c6ce7373062503
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-07-24 12:10:25 +0900

    :tada: solve baekjoon-1969

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bufferedReader.readLine());
-        ArrayList<Person> arrayList = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            Person person = new Person(stringTokenizer.nextToken(), Integer.parseInt(stringTokenizer.nextToken()), Integer.parseInt(stringTokenizer.nextToken()), Integer.parseInt(stringTokenizer.nextToken()));
-            arrayList.add(person);
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int M = Integer.parseInt(stringTokenizer.nextToken());
+        String[] array = new String[N];
+        for (int i = 0; i < N; i++) {
+            array[i] = bufferedReader.readLine();
         }
-        arrayList.sort((o1, o2) -> {
-            if (o1.year > o2.year) {
-                return 1;
-            } else if (o1.year == o2.year) {
-                if (o1.month > o2.month) {
-                    return 1;
-                } else if (o1.month == o2.month) {
-                    if (o1.day > o2.day) {
-                        return 1;
-                    }
-                } else {
-                    return -1;
-                }
-            } else {
-                return -1;
+        StringBuilder answer = new StringBuilder();
+        int equalCount = 0;
+        for (int i = 0; i < M; i++) {
+            HashMap<String, Integer> hashMap = new HashMap<>();
+            hashMap.put("A", 0);
+            hashMap.put("C", 0);
+            hashMap.put("G", 0);
+            hashMap.put("T", 0);
+            for (int j = 0; j < N; j++) {
+                String key = String.valueOf(array[j].charAt(i));
+                hashMap.replace(key, hashMap.get(key) + 1);
             }
-            return 0;
-        });
-        System.out.println(arrayList.get(arrayList.size() - 1).name);
-        System.out.println(arrayList.get(0).name);
-    }
+            int max = 0;
+            String c = "";
+            Set<String> set = hashMap.keySet();
 
-    public static class Person {
-        String name;
-        int day;
-        int month;
-        int year;
-
-        public Person(String name, int day, int month, int year) {
-            this.name = name;
-            this.day = day;
-            this.month = month;
-            this.year = year;
+            for (String o : set) {
+                if (max < hashMap.get(o)) {
+                    max = hashMap.get(o);
+                    c = o;
+                } else if (c.length() != 0 && max == hashMap.get(o)) {
+                    if (c.charAt(0) > o.charAt(0)) {
+                        c = o;
+                    }
+                }
+            }
+            equalCount += max;
+            answer.append(c);
         }
+        System.out.println(answer);
+        System.out.println(M * N - equalCount);
     }
 }
+
