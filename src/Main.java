a3b31fd06745263c746fef7d7d7ca4d1f0f5726c
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-07-25 13:00:38 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int S1 = Integer.parseInt(stringTokenizer.nextToken());
+        int S2 = Integer.parseInt(stringTokenizer.nextToken());
+        for (int i = 0; i < S1; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            if (!stringTokenizer.nextToken().equals(stringTokenizer.nextToken())) {
+                System.out.print("Wrong Answer");
+                return;
+            }
+        }
+        for (int i = 0; i < S2; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            if (!stringTokenizer.nextToken().equals(stringTokenizer.nextToken())) {
+                System.out.print("Why Wrong!!!");
+                return;
+            }
+        }
+        System.out.print("Accepted");
     }
 }
