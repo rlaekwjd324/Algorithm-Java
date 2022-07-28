0038d618bb6639c7c7cdbda3a06a38dbc182a6da
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-07-28 10:56:53 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stringBuilder = new StringBuilder();
+        int S = Integer.parseInt(bufferedReader.readLine());
+        String[] letters = {"A", "E", "I", "O", "U", "a", "e", "i", "o", "u"};
+        for (int i = 0; i < S; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int letterCount = 0;
+            int etcCount = 0;
+            while (stringTokenizer.hasMoreTokens()) {
+                char[] array = stringTokenizer.nextToken().toCharArray();
+                for (char c : array) {
+                    if (Arrays.asList(letters).contains(String.valueOf(c))) {
+                        letterCount++;
+                    } else {
+                        etcCount++;
+                    }
+                }
+            }
+            stringBuilder.append(etcCount).append(" ").append(letterCount).append("\n");
+        }
+        System.out.print(stringBuilder);
     }
 }
