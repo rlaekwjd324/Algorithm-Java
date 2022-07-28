8b5b9b8038d52514c977c1ca134b893059f0af5d
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-07-28 10:57:28 +0900

    :tada: solve baekjoon-11319

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int[] array = new int[3];
-        array[0] = Integer.parseInt(stringTokenizer.nextToken());
-        array[1] = Integer.parseInt(stringTokenizer.nextToken());
-        array[2] = Integer.parseInt(stringTokenizer.nextToken());
-        int n = Integer.parseInt(bufferedReader.readLine());
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = 0; j < 3; j++) {
-                stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-                for (int w = 0; w < 3; w++) {
-                    sum += Integer.parseInt(stringTokenizer.nextToken()) * array[w];
+        StringBuilder stringBuilder = new StringBuilder();
+        int S = Integer.parseInt(bufferedReader.readLine());
+        String[] letters = {"A", "E", "I", "O", "U", "a", "e", "i", "o", "u"};
+        for (int i = 0; i < S; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int letterCount = 0;
+            int etcCount = 0;
+            while (stringTokenizer.hasMoreTokens()) {
+                char[] array = stringTokenizer.nextToken().toCharArray();
+                for (char c : array) {
+                    if (Arrays.asList(letters).contains(String.valueOf(c))) {
+                        letterCount++;
+                    } else {
+                        etcCount++;
+                    }
                 }
             }
-            if (max < sum) {
-                max = sum;
-            }
+            stringBuilder.append(etcCount).append(" ").append(letterCount).append("\n");
         }
-        System.out.print(max);
+        System.out.print(stringBuilder);
     }
 }
