1695b41b2e07ed8ccd6c6554d9b9223b4d7bce7f
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-08-01 11:07:37 +0900

    :tada: solve baekjoon-4470

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int widthComputer = Integer.parseInt(stringTokenizer.nextToken());
-        int heightComputer = Integer.parseInt(stringTokenizer.nextToken());
-        int widthSticker = Integer.parseInt(stringTokenizer.nextToken());
-        int heightSticker = Integer.parseInt(stringTokenizer.nextToken());
-        if (widthComputer >= widthSticker + 2 && heightComputer >= heightSticker + 2) {
-            System.out.println("1");
-        } else {
-            System.out.println("0");
+        int N = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            stringBuilder.append(i).append(". ").append(bufferedReader.readLine()).append("\n");
         }
-
+        System.out.print(stringBuilder);
     }
 }
