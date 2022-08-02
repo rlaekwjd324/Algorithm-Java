0302a35c051685f5cbcf333e1998196a9c9deb9d
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-08-02 10:25:23 +0900

    :tada: solve baekjoon-13118

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bufferedReader.readLine());
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 1; i <= N; i++) {
-            stringBuilder.append(i).append(". ").append(bufferedReader.readLine()).append("\n");
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int[] array = new int[4];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(stringTokenizer.nextToken());
         }
-        System.out.print(stringBuilder);
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int x = Integer.parseInt(stringTokenizer.nextToken());
+        for (int i = 0; i < array.length; i++) {
+            if (x == array[i]) {
+                System.out.print(i + 1);
+                return;
+            }
+        }
+        System.out.print(0);
     }
 }
