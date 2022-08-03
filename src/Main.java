d74bd737ae94008d1c784c0d592f95703f2fdb5c
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-08-03 11:13:01 +0900

    :tada: solve baekjoon-4564

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int[] array = new int[4];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = Integer.parseInt(stringTokenizer.nextToken());
-        }
-        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int x = Integer.parseInt(stringTokenizer.nextToken());
-        for (int i = 0; i < array.length; i++) {
-            if (x == array[i]) {
-                System.out.print(i + 1);
-                return;
+        String str = bufferedReader.readLine();
+        StringBuilder stringBuilder = new StringBuilder();
+        while (!str.equals("0")) {
+            stringBuilder.append(str).append(" ");
+            while (str.length() > 1) {
+                int div = 1;
+                for (int i = 0; i < str.length(); i++) {
+                    div = div * (str.charAt(i) - '0');
+                }
+                str = String.valueOf(div);
+                stringBuilder.append(str).append(" ");
             }
+            stringBuilder.append("\n");
+            str = bufferedReader.readLine();
         }
-        System.out.print(0);
+        System.out.print(stringBuilder);
     }
 }
