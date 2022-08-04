dccfa70fe04d85802cbed96b1625dc5d88a39982
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-08-04 10:39:14 +0900

    :tada: solve baekjoon-17094

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,25 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String str = bufferedReader.readLine();
-        StringBuilder stringBuilder = new StringBuilder();
-        while (!str.equals("0")) {
-            stringBuilder.append(str).append(" ");
-            while (str.length() > 1) {
-                int div = 1;
-                for (int i = 0; i < str.length(); i++) {
-                    div = div * (str.charAt(i) - '0');
-                }
-                str = String.valueOf(div);
-                stringBuilder.append(str).append(" ");
+        int sLength = Integer.parseInt(bufferedReader.readLine());
+        String s = bufferedReader.readLine();
+        int count2 = 0;
+        int countE = 0;
+        for (int i = 0; i < sLength; i++) {
+            if (s.charAt(i) == '2') {
+                count2++;
+            } else {
+                countE++;
             }
-            stringBuilder.append("\n");
-            str = bufferedReader.readLine();
         }
-        System.out.print(stringBuilder);
+        if (count2 == countE) {
+            System.out.print("yee");
+            return;
+        }
+        if (count2 > countE) {
+            System.out.print("2");
+            return;
+        }
+        System.out.print("e");
     }
 }
