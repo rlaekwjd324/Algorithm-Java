39c501c3f499bab88c86051f789699fc976c8946
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-08-04 10:40:01 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int sLength = Integer.parseInt(bufferedReader.readLine());
+        String s = bufferedReader.readLine();
+        int count2 = 0;
+        int countE = 0;
+        for (int i = 0; i < sLength; i++) {
+            if (s.charAt(i) == '2') {
+                count2++;
+            } else {
+                countE++;
+            }
+        }
+        if (count2 == countE) {
+            System.out.print("yee");
+            return;
+        }
+        if (count2 > countE) {
+            System.out.print("2");
+            return;
+        }
+        System.out.print("e");
     }
 }
