b311d21255c399c9d80a998c7ef85b254bd393b3
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-08-05 11:34:19 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            String str = bufferedReader.readLine().replaceAll(" ", "");
+            int sum = 0;
+            for (int j = 0; j < str.length(); j++) {
+                sum += ((int) str.charAt(j) - 64);
+            }
+            if (sum == 100) {
+                stringBuilder.append("PERFECT LIFE").append("\n");
+            } else {
+                stringBuilder.append(sum).append("\n");
+            }
+        }
+        System.out.print(stringBuilder);
     }
 }
