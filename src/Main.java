a4cc0ed6098d7e0617ba4dbb83e108cb693ff973
KimDaJung <ekwjd324@gmail.com>
2022-08-06 15:09:29 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            String str = bufferedReader.readLine();
+            if (str.length() >= 6 && str.length() <= 9) {
+                stringBuilder.append("yes").append("\n");
+            } else {
+                stringBuilder.append("no").append("\n");
+            }
+        }
+        System.out.print(stringBuilder);
     }
 }
