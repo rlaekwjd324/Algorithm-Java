f91b9ecc54b1282da548f8709e174adbed06dc2e
KimDaJung (Dory) <54465767+rlaekwjd324@users.noreply.github.com>
2022-08-07 16:03:22 +0900

    :tada: solve baekjoon-1541

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,21 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bufferedReader.readLine());
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            String str = bufferedReader.readLine();
-            if (str.length() >= 6 && str.length() <= 9) {
-                stringBuilder.append("yes").append("\n");
+        String str = bufferedReader.readLine();
+        String[] array = str.split("-");
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            String[] tempArray = array[i].split("\\+");
+            int tempSum = 0;
+            for (String s : tempArray) {
+                tempSum += Integer.parseInt(s);
+            }
+            if (i == 0) {
+                sum += tempSum;
             } else {
-                stringBuilder.append("no").append("\n");
+                sum -= tempSum;
             }
         }
-        System.out.print(stringBuilder);
+        System.out.println(sum);
     }
 }
