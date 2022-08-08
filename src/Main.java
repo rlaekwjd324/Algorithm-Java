d4056b722e997b0c26ed85ad7c56ed9f2119cd58
KimDaJung (Dory) <54465767+rlaekwjd324@users.noreply.github.com>
2022-08-08 11:19:54 +0900

    :tada: solve baekjoon-2712

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String str = bufferedReader.readLine();
-        String[] array = str.split("-");
-        int sum = 0;
-        for (int i = 0; i < array.length; i++) {
-            String[] tempArray = array[i].split("\\+");
-            int tempSum = 0;
-            for (String s : tempArray) {
-                tempSum += Integer.parseInt(s);
+        int T = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            double num = Double.parseDouble(stringTokenizer.nextToken());
+            String str = stringTokenizer.nextToken();
+            switch (str) {
+                case "kg":
+                    stringBuilder.append(String.format("%.4f", num * 2.2046)).append(" lb");
+                    break;
+                case "lb":
+                    stringBuilder.append(String.format("%.4f", num * 0.4536)).append(" kg");
+                    break;
+                case "l":
+                    stringBuilder.append(String.format("%.4f", num * 0.2642)).append(" g");
+                    break;
+                case "g":
+                    stringBuilder.append(String.format("%.4f", num * 3.7854)).append(" l");
+                    break;
             }
-            if (i == 0) {
-                sum += tempSum;
-            } else {
-                sum -= tempSum;
-            }
+            stringBuilder.append("\n");
         }
-        System.out.println(sum);
+        System.out.print(stringBuilder);
     }
 }
