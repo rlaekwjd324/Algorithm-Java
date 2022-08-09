7ee1efebcfe0665f1f7d880cce0a73461855788e
KimDaJung (Dory) <54465767+rlaekwjd324@users.noreply.github.com>
2022-08-09 13:30:42 +0900

    :tada: solve baekjoon-20232

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(bufferedReader.readLine());
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < T; i++) {
-            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            double num = Double.parseDouble(stringTokenizer.nextToken());
-            String str = stringTokenizer.nextToken();
-            switch (str) {
-                case "kg":
-                    stringBuilder.append(String.format("%.4f", num * 2.2046)).append(" lb");
-                    break;
-                case "lb":
-                    stringBuilder.append(String.format("%.4f", num * 0.4536)).append(" kg");
-                    break;
-                case "l":
-                    stringBuilder.append(String.format("%.4f", num * 0.2642)).append(" g");
-                    break;
-                case "g":
-                    stringBuilder.append(String.format("%.4f", num * 3.7854)).append(" l");
-                    break;
-            }
-            stringBuilder.append("\n");
-        }
-        System.out.print(stringBuilder);
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        hashMap.put(1995, "ITMO");
+        hashMap.put(1996, "SPbSU");
+        hashMap.put(1997, "SPbSU");
+        hashMap.put(1998, "ITMO");
+        hashMap.put(1999, "ITMO");
+        hashMap.put(2000, "SPbSU");
+        hashMap.put(2001, "ITMO");
+        hashMap.put(2002, "ITMO");
+        hashMap.put(2003, "ITMO");
+        hashMap.put(2004, "ITMO");
+        hashMap.put(2005, "ITMO");
+        hashMap.put(2006, "PetrSU, ITMO");
+        hashMap.put(2007, "SPbSU");
+        hashMap.put(2008, "SPbSU");
+        hashMap.put(2009, "ITMO");
+        hashMap.put(2010, "ITMO");
+        hashMap.put(2011, "ITMO");
+        hashMap.put(2012, "ITMO");
+        hashMap.put(2013, "SPbSU");
+        hashMap.put(2014, "ITMO");
+        hashMap.put(2015, "ITMO");
+        hashMap.put(2016, "ITMO");
+        hashMap.put(2017, "ITMO");
+        hashMap.put(2018, "SPbSU");
+        hashMap.put(2019, "ITMO");
+        System.out.print(hashMap.get(Integer.parseInt(bufferedReader.readLine())));
     }
 }
