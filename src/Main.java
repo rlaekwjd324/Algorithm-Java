014737a7bb4a7c5fe42b6a3423c64484962d1e60
KimDaJung (Dory) <54465767+rlaekwjd324@users.noreply.github.com>
2022-08-10 13:25:35 +0900

    :tada: solve baekjoon-9455

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        HashMap<Integer, String> hashMap = new HashMap<>();
-        hashMap.put(1995, "ITMO");
-        hashMap.put(1996, "SPbSU");
-        hashMap.put(1997, "SPbSU");
-        hashMap.put(1998, "ITMO");
-        hashMap.put(1999, "ITMO");
-        hashMap.put(2000, "SPbSU");
-        hashMap.put(2001, "ITMO");
-        hashMap.put(2002, "ITMO");
-        hashMap.put(2003, "ITMO");
-        hashMap.put(2004, "ITMO");
-        hashMap.put(2005, "ITMO");
-        hashMap.put(2006, "PetrSU, ITMO");
-        hashMap.put(2007, "SPbSU");
-        hashMap.put(2008, "SPbSU");
-        hashMap.put(2009, "ITMO");
-        hashMap.put(2010, "ITMO");
-        hashMap.put(2011, "ITMO");
-        hashMap.put(2012, "ITMO");
-        hashMap.put(2013, "SPbSU");
-        hashMap.put(2014, "ITMO");
-        hashMap.put(2015, "ITMO");
-        hashMap.put(2016, "ITMO");
-        hashMap.put(2017, "ITMO");
-        hashMap.put(2018, "SPbSU");
-        hashMap.put(2019, "ITMO");
-        System.out.print(hashMap.get(Integer.parseInt(bufferedReader.readLine())));
+        int T = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int m = Integer.parseInt(stringTokenizer.nextToken());
+            int n = Integer.parseInt(stringTokenizer.nextToken());
+            int[][] array = new int[m][n];
+            for (int j = 0; j < m; j++) {
+                stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+                for (int k = 0; k < n; k++) {
+                    array[j][k] = Integer.parseInt(stringTokenizer.nextToken());
+                }
+            }
+            int sum = 0;
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (array[j][k] == 1) {
+                        for (int l = j + 1; l < m; l++) {
+                            if (array[l][k] == 0) {
+                                sum++;
+                            }
+                        }
+                    }
+                }
+            }
+            stringBuilder.append(sum).append("\n");
+        }
+        System.out.print(stringBuilder);
     }
 }
