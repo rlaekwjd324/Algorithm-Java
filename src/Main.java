54890a3c9eadcb2367d1ce6b1abc4a9424c96026
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-08-11 10:19:15 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String str = bufferedReader.readLine();
+        StringBuilder stringBuilder = new StringBuilder();
+        while (!str.equals("***")) {
+            StringBuilder tempStringBuilder = new StringBuilder();
+            tempStringBuilder.append(str);
+            stringBuilder.append(tempStringBuilder.reverse()).append("\n");
+            str = bufferedReader.readLine();
+        }
+        System.out.print(stringBuilder);
     }
 }
