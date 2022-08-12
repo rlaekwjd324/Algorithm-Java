d331fe84f995c06001f0a35ac0613732e85fab47
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-08-12 15:19:42 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 0; i < N; i++) {
+            String str = bufferedReader.readLine();
+            if (str.equals("anj")) {
+                System.out.print("뭐야;");
+                return;
+            }
+        }
+        System.out.print("뭐야?");
     }
 }
