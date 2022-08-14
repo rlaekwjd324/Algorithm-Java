5df58a5423828cb1ef47a4a884b45129c37d9530
KimDaJung (Dory) <54465767+rlaekwjd324@users.noreply.github.com>
2022-08-14 15:12:59 +0900

    :tada: solve baekjoon-2587

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bufferedReader.readLine());
-        for (int i = 0; i < N; i++) {
-            String str = bufferedReader.readLine();
-            if (str.equals("anj")) {
-                System.out.print("뭐야;");
-                return;
-            }
+        int[] array = new int[5];
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            array[i] = Integer.parseInt(bufferedReader.readLine());
+            sum += array[i];
         }
-        System.out.print("뭐야?");
+        Arrays.sort(array);
+        System.out.println(sum / 5);
+        System.out.println(array[2]);
     }
 }
