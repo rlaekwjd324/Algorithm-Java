df423f8f30900422a7a1b811898576c854bf7cfd
KimDaJung <ekwjd324@gmail.com>
2022-08-14 15:12:23 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[5];
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            array[i] = Integer.parseInt(bufferedReader.readLine());
+            sum += array[i];
+        }
+        Arrays.sort(array);
+        System.out.println(sum / 5);
+        System.out.println(array[2]);
     }
 }
