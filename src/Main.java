179f76093f5bb49550d3f16daf86a42840735a1b
KimDaJung <ekwjd324@gmail.com>
2022-08-14 15:09:06 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String str = bufferedReader.readLine();
+        if (str.contains("D2") || str.contains("d2")) {
+            System.out.print("D2");
+            return;
+        }
+        System.out.print("unrated");
     }
 }
