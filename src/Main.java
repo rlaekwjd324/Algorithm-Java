e1f8d268a6f0aa815e408f20bbb80d240221c54f
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-08-16 10:17:38 +0900

    :tada: solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < N * 5; i++) {
+            if (i >= N * 5 - N) {
+                stringBuilder.append("@".repeat(Math.max(0, N * 5)));
+            } else {
+                stringBuilder.append("@".repeat(Math.max(0, N)));
+            }
+            stringBuilder.append("\n");
+        }
+        System.out.print(stringBuilder);
     }
 }
