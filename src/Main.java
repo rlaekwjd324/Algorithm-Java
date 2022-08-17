47be84f888cd74d8e6eb952138c852ee1fbf2506
KimDaJung (Dory) <54465767+rlaekwjd324@users.noreply.github.com>
2022-08-17 09:57:46 +0900

    :tada: solve baekjoon-21312

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bufferedReader.readLine());
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < N * 5; i++) {
-            if (i >= N * 5 - N) {
-                stringBuilder.append("@".repeat(Math.max(0, N * 5)));
-            } else {
-                stringBuilder.append("@".repeat(Math.max(0, N)));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int a = Integer.parseInt(stringTokenizer.nextToken());
+        int b = Integer.parseInt(stringTokenizer.nextToken());
+        int c = Integer.parseInt(stringTokenizer.nextToken());
+        int[] array = {a, b, c, a * b, b * c, c * a, a * b * c};
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (max % 2 == 0 && array[i] % 2 == 1) {
+                max = array[i];
+            } else if (max % 2 == 0 || array[i] % 2 == 1) {
+                max = Math.max(max, array[i]);
             }
-            stringBuilder.append("\n");
         }
-        System.out.print(stringBuilder);
+        System.out.print(max);
     }
 }
