ea75368f49345f1ddaa25a2126c5e80b73288743
KimDaJung (Dory) <54465767+rlaekwjd324@users.noreply.github.com>
2022-08-18 10:20:49 +0900

    :tada: solve baekjoon-23794

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int a = Integer.parseInt(stringTokenizer.nextToken());
-        int b = Integer.parseInt(stringTokenizer.nextToken());
-        int c = Integer.parseInt(stringTokenizer.nextToken());
-        int[] array = {a, b, c, a * b, b * c, c * a, a * b * c};
-        int max = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (max % 2 == 0 && array[i] % 2 == 1) {
-                max = array[i];
-            } else if (max % 2 == 0 || array[i] % 2 == 1) {
-                max = Math.max(max, array[i]);
+        int N = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < N + 2; i++) {
+            if (i == 0 || i == N + 1) {
+                stringBuilder.append("@".repeat(N + 2));
+            } else {
+                stringBuilder.append("@");
+                stringBuilder.append(" ".repeat(N));
+                stringBuilder.append("@");
             }
+            stringBuilder.append("\n");
         }
-        System.out.print(max);
+        System.out.print(stringBuilder);
     }
 }
