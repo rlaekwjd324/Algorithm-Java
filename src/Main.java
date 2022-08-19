219b5b4fe748c65e4cbb60973100716195f1762c
KimDaJung (Dory) <54465767+rlaekwjd324@users.noreply.github.com>
2022-08-20 08:36:31 +0900

    :tada: solve baekjoon-8545

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,9 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bufferedReader.readLine());
+        BufferedReader bufferedReader  = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < N * 5; i++) {
-            if (i < N || i >= N * 4) {
-                stringBuilder.append("@".repeat(Math.max(0, N * 5)));
-            } else {
-                for (int j = 0; j < N * 5; j++) {
-                    if (j < N || j >= N * 4) {
-                        stringBuilder.append("@");
-                    } else {
-                        stringBuilder.append(" ");
-                    }
-                }
-            }
-            stringBuilder.append("\n");
-        }
-        System.out.print(stringBuilder);
+        stringBuilder.append(bufferedReader.readLine());
+        System.out.println(stringBuilder.reverse());
     }
 }
