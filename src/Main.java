075f4117ba074911a1df3726c4c710159d870d20
KimDaJung <ekwjd324@gmail.com>
2022-08-20 08:33:21 +0900

    :

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int[] num = new int[4];
+        for (int i = 0; i < 4; i++) {
+            num[i] = Integer.parseInt(bufferedReader.readLine());
+        }
+        if (num[0] == num[1] && num[1] == num[2] && num[2] == num[3]) {
+            System.out.println("Constant Depth");
+            return;
+        }
+        if (num[0] >= num[1] && num[1] >= num[2] && num[2] >= num[3]) {
+            System.out.println("Fish Rising");
+            return;
+        }
+        if (num[0] <= num[1] && num[1] <= num[2] && num[2] <= num[3]) {
+            System.out.println("Fish Diving");
+            return;
+        }
+        System.out.println("No Fish.");
     }
 }
